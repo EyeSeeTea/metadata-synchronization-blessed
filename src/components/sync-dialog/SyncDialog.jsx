331d1c6a270833871df910e010ce5ab2570a000b
Matias Garcia <matiasigarcia91@gmail.com>
2019-04-08 13:35:30 +0200

    Reset syncDialog state after sync

--- src/components/sync-dialog/SyncDialog.jsx
+++ src/components/sync-dialog/SyncDialog.jsx
@@ -57,7 +57,7 @@ class SyncDialog extends React.Component {
             console.error(error);
             this.props.handleClose();
         }
-
+        this.setState({ targetInstances: [], instanceOptions: [] });
         this.props.loading.reset();
     };
 
