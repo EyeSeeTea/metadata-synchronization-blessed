41148e81dedc5932d3a3aed6ea1aa5c9b1886962
Alexis Rico <sferadev@gmail.com>
2019-09-12 07:04:24 +0200

    Add extra help

--- src/components/rules-creation-page/SyncRulesWizard.jsx
+++ src/components/rules-creation-page/SyncRulesWizard.jsx
@@ -50,17 +50,27 @@ class SyncRulesWizard extends React.Component {
             component: SchedulerStep,
             validationKeys: ["frequency", "enabled"],
             description: i18n.t("Configure the scheduling frequency for the synchronization rule"),
-            help: i18n.t(
-                `This step allows to schedule background metadata synchronization jobs in a remote server.
-                 You can either select a pre-defined frequency from the drop-down menu or you enter a custom cron expression.\n
-                 A cron expression is a string comprising six fields separated by white space that represents a routine.\n
-                 Second (0 - 59)\n
-                 Minute (0 - 59)\n
-                 Hour (0 - 23)\n
-                 Day of the month (1 - 31)\n
-                 Month (1 - 12)\n
-                 Day of the week (1 - 7) (Monday to Sunday)\n`
-            ),
+            help: [
+                i18n.t(
+                    "This step allows to schedule background metadata synchronization jobs in a remote server."
+                ),
+                i18n.t(
+                    "You can either select a pre-defined frequency from the drop-down menu or you enter a custom cron expression."
+                ),
+                "A cron expression is a string comprising six fields separated by white space that represents a routine.",
+                i18n.t("Second (0 - 59)"),
+                i18n.t("Minute (0 - 59)"),
+                i18n.t("Hour (0 - 23)"),
+                i18n.t("Day of the month (1 - 31)"),
+                i18n.t("Month (1 - 12)"),
+                i18n.t("Day of the week (1 - 7) (Monday to Sunday)"),
+                i18n.t(
+                    "An asterisk (*) matches all possibilities. For instance, if we want to run a rule every day we would use asterisks for day of the month, day of the week, and month of the year to match all values."
+                ),
+                i18n.t(
+                    "A wildcard (?) means no specific value and only works for day of the month or day of the week. For example, if you want to execute a rule on a particular day (10th) but you don't care about what day of the week that is, you would use ? in the day of the week field."
+                ),
+            ].join("\n"),
         },
         {
             key: "summary",
