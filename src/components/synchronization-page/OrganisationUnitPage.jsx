ad2b726ae64b08bc74e6680a79422a95d67b2a6a
Alexis Rico <sferadev@gmail.com>
2019-09-17 12:44:14 +0200

    Refactor select org unit children

--- src/components/synchronization-page/OrganisationUnitPage.jsx
+++ src/components/synchronization-page/OrganisationUnitPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import i18n from "@dhis2/d2-i18n";
-
 import {
     OrganisationUnitGroupModel,
     OrganisationUnitGroupSetModel,
     OrganisationUnitModel,
 } from "../../models/d2Model";
-import { getOrgUnitSubtree } from "../../logic/metadata";
 import GenericSynchronizationPage from "./GenericSynchronizationPage";
 
 export default class OrganisationUnitPage extends React.Component {
@@ -15,59 +13,13 @@ export default class OrganisationUnitPage extends React.Component {
         d2: PropTypes.object.isRequired,
     };
 
-    state = {
-        children: [],
-        metadataTableKey: Math.random(),
-    };
-
     models = [OrganisationUnitModel, OrganisationUnitGroupModel, OrganisationUnitGroupSetModel];
-
-    onSelectionChange = children => this.setState({ children });
-
-    selectChildren = async selectedOUs => {
-        const { d2 } = this.props;
-        const { children } = this.state;
-
-        const ids = new Set(children);
-        for (const selectedOU of selectedOUs) {
-            const subtree = await getOrgUnitSubtree(d2, selectedOU.id);
-            subtree.forEach(id => ids.add(id));
-        }
-        this.setState({ children: Array.from(ids), metadataTableKey: Math.random() });
-    };
-
-    actions = [
-        {
-            name: "details",
-            text: i18n.t("Details"),
-            multiple: false,
-            type: "details",
-        },
-        {
-            name: "select-children",
-            text: i18n.t("Select with children subtree"),
-            multiple: true,
-            onClick: this.selectChildren,
-            icon: "done_all",
-        },
-    ];
 
     render() {
         const { d2 } = this.props;
-        const { children, metadataTableKey } = this.state;
 
         const title = i18n.t("Organisation Units Synchronization");
 
-        return (
-            <GenericSynchronizationPage
-                key={metadataTableKey}
-                d2={d2}
-                models={this.models}
-                title={title}
-                actions={this.actions}
-                initialSelection={children}
-                onSelectionChange={this.onSelectionChange}
-            />
-        );
+        return <GenericSynchronizationPage d2={d2} models={this.models} title={title} />;
     }
 }
