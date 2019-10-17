90f154f7f4dc1c1c6627c2d411bbf3eb5adc77fe
Jorge Sánchez <xurxosf@gmail.com>
2019-10-17 13:28:36 +0200

    create d2ModelFactory test

---
+++ src/models/__tests__/d2ModelFactory.spec.js
@@ -0,0 +1,16 @@
+import {d2ModelFactory} from "../d2ModelFactory";
+import {DataElementGroupModel} from "../d2Model"
+
+describe("d2ModelFactory", () => {
+    describe("d2ModelFactory should return specific model", () => {
+        it("DataElementGroup", async () => {
+            const d2Stub = { models: { dataElementGroups: { name: "dataElementGroup" } } };
+
+            const d2Model = d2ModelFactory(d2Stub, "dataElementGroups");
+
+            expect(d2Model).toEqual(DataElementGroupModel);
+        });
+    });
+});
+
+export {};
