d4f4112011d828f2a4ee86b2593289bb674ec984
Adrian Quintana <adrianquintana@gmail.com>
2019-10-18 10:22:06 +0100

    lint

--- src/models/d2ModelFactory.ts
+++ src/models/d2ModelFactory.ts
@@ -48,6 +48,9 @@ export const metadataModels = Object.values(classes);
 export function d2ModelFactory(d2: D2, d2ModelName: string): typeof D2Model {
     const modelName = d2.models[d2ModelName].name;
     const className = modelName.charAt(0).toUpperCase() + modelName.slice(1) + "Model";
-    console.debug(`d2ModelFactory for modelName ${d2ModelName} return ` + (!classes[className]?`defaultModel`:className));
+    console.debug(
+        `d2ModelFactory for modelName ${d2ModelName} return ` +
+            (!classes[className] ? `defaultModel` : className)
+    );
     return classes[className] || defaultModel(modelName);
 }
