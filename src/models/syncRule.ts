ea7ce9615305d687a03fbe47c641bfbbc7d9a905
Jorge Sánchez <xurxosf@gmail.com>
2019-10-22 08:08:51 +0200

    Format code and update i18n

--- src/models/syncRule.ts
+++ src/models/syncRule.ts
@@ -118,7 +118,6 @@ export default class SyncRule {
     }
 
     public static createOnDemand(): SyncRule {
-
         return SyncRule.create().updateName("On-demand");
     }
 
@@ -271,47 +270,47 @@ export default class SyncRule {
             name: _.compact([
                 !this.name.trim()
                     ? {
-                        key: "cannot_be_blank",
-                        namespace: { field: "name" },
-                    }
+                          key: "cannot_be_blank",
+                          namespace: { field: "name" },
+                      }
                     : null,
             ]),
             metadataIds: _.compact([
                 this.metadataIds.length === 0
                     ? {
-                        key: "cannot_be_empty",
-                        namespace: { element: "metadata element" },
-                    }
+                          key: "cannot_be_empty",
+                          namespace: { element: "metadata element" },
+                      }
                     : null,
             ]),
             targetInstances: _.compact([
                 this.targetInstances.length === 0
                     ? {
-                        key: "cannot_be_empty",
-                        namespace: { element: "instance" },
-                    }
+                          key: "cannot_be_empty",
+                          namespace: { element: "instance" },
+                      }
                     : null,
             ]),
             frequency: _.compact([
                 this.frequency && !isValidCronExpression(this.frequency)
                     ? {
-                        key: "cron_expression_must_be_valid",
-                        namespace: { expression: "frequency" },
-                    }
+                          key: "cron_expression_must_be_valid",
+                          namespace: { expression: "frequency" },
+                      }
                     : null,
             ]),
             enabled: _.compact([
                 this.enabled && !isValidCronExpression(this.frequency)
                     ? {
-                        key: "cannot_enable_without_valid",
-                        namespace: { expression: "frequency" },
-                    }
+                          key: "cannot_enable_without_valid",
+                          namespace: { expression: "frequency" },
+                      }
                     : null,
             ]),
         });
     }
 
-    public async isValid(): Promise<Boolean>{
+    public async isValid(): Promise<Boolean> {
         const validation = await this.validate();
         return _.flatten(Object.values(validation)).length == 0;
     }
