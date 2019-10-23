87c63e8f10b23f11cf34d1daf973acacb1a5b71a
Jorge Sánchez <xurxosf@gmail.com>
2019-10-23 14:06:08 +0200

    Load orgUnits in OrganisationUnitsSelectionStep

--- src/components/wizard/data/steps/OrganisationUnitsSelectionStep.tsx
+++ src/components/wizard/data/steps/OrganisationUnitsSelectionStep.tsx
@@ -1,33 +1,62 @@
 import React from "react";
-import { withSnackbar } from "d2-ui-components";
-
-import MetadataTable from "../../../metadata-table/MetadataTable";
-import { D2Api } from "../../../../types/d2";
+import { withSnackbar, OrgUnitsSelector } from "d2-ui-components";
 import SyncRule from "../../../../models/syncRule";
+import { D2 } from "../../../../types/d2";
+import { getCurrentUserOrganisationUnits } from "../../../../utils/d2";
+import _ from "lodash";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 interface OrganisationUnitsStepProps {
-    d2: D2Api;
+    d2: D2;
     syncRule: SyncRule;
-    //snackbar: PropTypes.object.isRequired,
+    snackbar: any;
     onChange: () => void;
 }
 
-const changeSelection = (metadataIds: string[]) => {
-    console.log(`change selection ${metadataIds}`);
+const OrganisationUnitsSelectionStep: React.FC<OrganisationUnitsStepProps> = ({
+    d2,
+    // syncRule,
+    // snackbar,
+    // onChange
+}) => {
+    const [organisationUnitsRootIds, setOrganisationUnitsRootIds] = React.useState<string[]>([]);
+    const [selectedOrganisationUnits, setSelectedOrganisationUnits] = React.useState<string[]>([]);
+
+    React.useEffect(() => {
+        const retrieveOrganisationUnitsRootIds = async () => {
+            const organisationUnitsRootIds = await getCurrentUserOrganisationUnits(d2);
+            setOrganisationUnitsRootIds(organisationUnitsRootIds);
+        };
+
+        retrieveOrganisationUnitsRootIds();
+    }, [d2]);
+
+    const changeSelection = (orgUnitsPaths: string[]) => {
+        setSelectedOrganisationUnits(orgUnitsPaths);
+        //onChange(syncRule.updateData(orgUnitsPaths));
+        console.log(`change selection ${orgUnitsPaths}`);
+    };
+
+    console.log({ organisationUnitsRootIds });
+
+    if (_.isEmpty(organisationUnitsRootIds)) {
+        return (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <CircularProgress />
+            </div>
+        );
+    } else {
+        return (
+            <OrgUnitsSelector
+                d2={d2}
+                onChange={changeSelection}
+                selected={selectedOrganisationUnits}
+                //levels={[1, 2]}
+                rootIds={organisationUnitsRootIds}
+                //listParams={{ maxLevel: 4 }}
+            />
+        );
+    }
 };
 
-export default function OrganisationUnitsSelectionStep(props: OrganisationUnitsStepProps) {
-    const { d2, syncRule, ...rest } = props;
-
-    const component = (
-        <MetadataTable
-            d2={d2}
-            notifyNewSelection={changeSelection}
-            initialSelection={[]}
-            models={[]}
-            {...rest}
-        />
-    );
-
-    return withSnackbar(component);
-}
+export default withSnackbar(OrganisationUnitsSelectionStep);
