a09b1d80801fdb6369c99a052a5dba2a369c50a6
Alexis Rico <sferadev@gmail.com>
2020-01-05 12:49:02 +0100

    Adapt landing page tests to the new proposal

--- cypress/integration/landing-page.spec.js
+++ cypress/integration/landing-page.spec.js
@@ -7,7 +7,7 @@ context("Landing page", () => {
     });
 
     it("renders a table with all pages of the application", () => {
-        cy.get(dataTest("pages"))
+        cy.get(dataTest("div-pages"))
             .should("have.length", 1)
             .should("be.visible");
 
@@ -16,92 +16,96 @@ context("Landing page", () => {
         cy.contains("Events Sync");
         cy.contains("Configuration");
 
-        cy.get(dataTest("metadata")).contains("Manual sync");
-        cy.get(dataTest("metadata")).contains("Sync rules");
-        cy.get(dataTest("metadata")).contains("History");
+        cy.get(dataTest("div-pages-metadata")).contains("Manual sync");
+        cy.get(dataTest("div-pages-metadata")).contains("Sync rules");
+        cy.get(dataTest("div-pages-metadata")).contains("History");
 
-        cy.get(dataTest("aggregated")).contains("Manual sync");
-        cy.get(dataTest("aggregated")).contains("Sync rules");
-        cy.get(dataTest("aggregated")).contains("History");
+        cy.get(dataTest("div-pages-aggregated")).contains("Manual sync");
+        cy.get(dataTest("div-pages-aggregated")).contains("Sync rules");
+        cy.get(dataTest("div-pages-aggregated")).contains("History");
 
-        cy.get(dataTest("events")).contains("Manual sync");
-        cy.get(dataTest("events")).contains("Sync rules");
-        cy.get(dataTest("events")).contains("History");
+        cy.get(dataTest("div-pages-events")).contains("Manual sync");
+        cy.get(dataTest("div-pages-events")).contains("Sync rules");
+        cy.get(dataTest("div-pages-events")).contains("History");
 
-        cy.get(dataTest("configuration")).contains("Destination instance settings");
+        cy.get(dataTest("div-pages-configuration")).contains("Destination instance settings");
     });
 
     // Metadata
     it("enters the Metadata manual sync page", function() {
-        cy.get("[data-test='card-metadata-0'] button[title='List']").click();
+        cy.get(dataTest("MenuCard-metadata-manual-sync", "button[title='List']")).click();
         cy.get(dataTest("page-header-title")).contains("Metadata Synchronization");
     });
 
     it("enters the Metadata sync rules page", function() {
-        cy.get("[data-test='card-metadata-1'] button[title='List']").click();
+        cy.get(dataTest("MenuCard-metadata-sync-rules", "button[title='List']")).click();
         cy.get(dataTest("page-header-title")).contains("Metadata Synchronization Rules");
     });
 
     it("enters the Metadata sync rules page", function() {
-        cy.get("[data-test='card-metadata-1'] button[title='Add']").click();
+        cy.get(dataTest("MenuCard-metadata-sync-rules", "button[title='Add']")).click();
         cy.get(dataTest("page-header-title")).contains("New metadata synchronization rule");
     });
 
     it("enters the Metadata history page", function() {
-        cy.get("[data-test='card-metadata-2'] button[title='List']").click();
+        cy.get(dataTest("MenuCard-metadata-history", "button[title='List']")).click();
         cy.get(dataTest("page-header-title")).contains("Metadata Synchronization History");
     });
 
     // Aggregated Data
     it("enters the Aggregated Data manual sync page", function() {
-        cy.get("[data-test='card-aggregated-0'] button[title='List']").click();
+        cy.get(dataTest("MenuCard-aggregated-manual-sync", "button[title='List']")).click();
         cy.get(dataTest("page-header-title")).contains("Aggregated Data Synchronization");
     });
 
     it("enters the Aggregated Data sync rules page", function() {
-        cy.get("[data-test='card-aggregated-1'] button[title='List']").click();
+        cy.get(dataTest("MenuCard-aggregated-sync-rules", "button[title='List']")).click();
         cy.get(dataTest("page-header-title")).contains("Aggregated Data Synchronization Rules");
     });
 
     it("enters the Aggregated Data sync rules page", function() {
-        cy.get("[data-test='card-aggregated-1'] button[title='Add']").click();
+        cy.get(dataTest("MenuCard-aggregated-sync-rules", "button[title='Add']")).click();
         cy.get(dataTest("page-header-title")).contains("New aggregated synchronization rule");
     });
 
     it("enters the Aggregated Data history page", function() {
-        cy.get("[data-test='card-aggregated-2'] button[title='List']").click();
+        cy.get(dataTest("MenuCard-aggregated-history", "button[title='List']")).click();
         cy.get(dataTest("page-header-title")).contains("Aggregated Data Synchronization History");
     });
 
     // Events
     it("enters the Events manual sync page", function() {
-        cy.get("[data-test='card-events-0'] button[title='List']").click();
+        cy.get(dataTest("MenuCard-events-manual-sync", "button[title='List']")).click();
         cy.get(dataTest("page-header-title")).contains("Events Synchronization");
     });
 
     it("enters the Events sync rules page", function() {
-        cy.get("[data-test='card-events-1'] button[title='List']").click();
+        cy.get(dataTest("MenuCard-events-sync-rules", "button[title='List']")).click();
         cy.get(dataTest("page-header-title")).contains("Events Synchronization Rules");
     });
 
     it("enters the Aggregated Data sync rules page", function() {
-        cy.get("[data-test='card-events-1'] button[title='Add']").click();
+        cy.get(dataTest("MenuCard-events-sync-rules", "button[title='Add']")).click();
         cy.get(dataTest("page-header-title")).contains("New events synchronization rule");
     });
 
     it("enters the Aggregated Data history page", function() {
-        cy.get("[data-test='card-events-2'] button[title='List']").click();
+        cy.get(dataTest("MenuCard-events-history", "button[title='List']")).click();
         cy.get(dataTest("page-header-title")).contains("Events Synchronization History");
     });
 
     // Instance Settings
     it("enters the Destination instance settings page", function() {
-        cy.get("[data-test='card-configuration-0'] button[title='List']").click();
+        cy.get(
+            dataTest("MenuCard-configuration-destination-instance-settings", "button[title='List']")
+        ).click();
         cy.get(dataTest("page-header-title")).contains("Destination Instance Settings");
     });
 
     it("enters the Destination instance creation page", function() {
-        cy.get("[data-test='card-configuration-0'] button[title='Add']").click();
+        cy.get(
+            dataTest("MenuCard-configuration-destination-instance-settings", "button[title='Add']")
+        ).click();
         cy.get(dataTest("page-header-title")).contains("New Instance");
     });
 });
