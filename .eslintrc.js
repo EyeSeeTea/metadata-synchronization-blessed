4ee8acb257efb667547aa91857e94e59ff848835
Jorge Sánchez <xurxosf@gmail.com>
2020-01-30 08:22:19 +0100

    Disable no-use-before-define rule for functions: for tests files is more readable view first tests and then helper functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,10 @@ module.exports = {
         "@typescript-eslint/indent": "off",
         "@typescript-eslint/member-delimiter-style": "off",
         "@typescript-eslint/type-annotation-spacing": "off",
+        "@typescript-eslint/no-use-before-define": [
+            "error",
+            { functions: false, classes: true, variables: true },
+        ],
         "no-extra-semi": "off",
         "no-mixed-spaces-and-tabs": "off",
         "react-hooks/rules-of-hooks": "error",
