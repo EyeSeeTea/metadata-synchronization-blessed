5c5a84c94ef59646fe99e85e3b2c906a0ddd7dea
Alexis Rico <sferadev@gmail.com>
2020-02-03 14:45:29 +0100

    Fix tests

--- src/models/__tests__/d2ModelFactory.spec.js
+++ src/models/__tests__/d2ModelFactory.spec.js
@@ -4,7 +4,7 @@ import { DataElementGroupModel } from "../d2Model";
 describe("d2ModelFactory", () => {
     describe("d2ModelFactory should return specific model", () => {
         it("DataElementGroup", async () => {
-            const apiStub = { models: { dataElementGroups: { modelName: "dataElementGroup" } } };
+            const apiStub = { models: { dataElementGroups: { modelName: "dataElementGroups" } } };
             const d2Model = d2ModelFactory(apiStub, "dataElementGroups");
             expect(d2Model).toEqual(DataElementGroupModel);
         });
