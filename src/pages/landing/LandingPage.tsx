f0ca58152e19d455d627ddfdabaec1dd2656befe
Adrian Quintana <adrianquintana@gmail.com>
2020-02-06 21:07:31 +0000

    change help text for instances access. Go back to instances list instead of root page

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -150,7 +150,7 @@ const LandingPage: React.FC = () => {
                 {
                     name: i18n.t("Destination instance settings"),
                     description: i18n.t(
-                        "Create, check connectivity, modify and delete DHIS2 destination instances."
+                        "Create, check connectivity, modify and delete DHIS2 destination instances. Map metadata objects between instances."
                     ),
                     addAction: showCreateLinks ? () => history.push("/instances/new") : undefined,
                     listAction: () => history.push("/instances"),
