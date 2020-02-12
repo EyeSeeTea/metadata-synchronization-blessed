44a318500b90cdb008ee56cd5b1940530160c173
Alexis Rico <sferadev@gmail.com>
2020-02-13 00:38:44 +0100

    Update commands from project-monitoring

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,12 +1,22 @@
 // <reference types="Cypress" />
 /* global Cypress, cy */
 
-import { externalUrl, generateFixtures, stubBackend } from "./network-fixtures";
 import _ from "lodash";
 
+const externalUrl = Cypress.env("EXTERNAL_API");
+const appUrl = Cypress.env("ROOT_URL");
 const dhis2AuthEnvValue = Cypress.env("DHIS2_AUTH");
+
 if (!dhis2AuthEnvValue) {
     throw new Error("CYPRESS_DHIS2_AUTH=user1:pass1[,user2:pass2,...] not set");
+}
+
+if (!externalUrl) {
+    throw new Error("CYPRESS_EXTERNAL_API not set");
+}
+
+if (!appUrl) {
+    throw new Error("CYPRESS_ROOT_URL not set");
 }
 
 const dhis2Auth = _(dhis2AuthEnvValue)
@@ -15,46 +25,47 @@ const dhis2Auth = _(dhis2AuthEnvValue)
     .fromPairs()
     .value();
 
-Cypress.Cookies.defaults({
-    whitelist: "JSESSIONID",
-});
-
 Cypress.Commands.add("login", (username, _password = null) => {
-    // Start server and create fixture for the encryption key
-    cy.server();
-    cy.fixture("app-config.json").then(json => cy.route("GET", "app-config.json", json));
-
     const password = _password || dhis2Auth[username];
-    if (stubBackend) {
-        cy.log(
-            "Stubbing all backend network requests - unmatched requests will automatically fail"
-        );
-    } else {
-        cy.log(`Performing end-to-end test with API server URL '${externalUrl}'`);
-        if (generateFixtures) {
-            cy.log("Generating fixtures from end-to-end network requests");
-        }
-    }
-    if (!stubBackend) {
-        cy.log("Login", { username });
-        cy.request({
-            method: "GET",
-            url: `${externalUrl}/api/me`,
-            auth: {
-                user: username,
-                pass: password,
-            },
-            log: true,
-        });
-    }
+
+    cy.log("Login", { username, password });
+    cy.request({
+        method: "POST",
+        url: `${externalUrl}/dhis-web-commons-security/login.action`,
+        body: {
+            /* eslint-disable @typescript-eslint/camelcase*/
+            j_username: username,
+            j_password: password,
+        },
+        form: true,
+        log: true,
+    });
 });
 
-Cypress.on("window:before:load", win => {
-    win.fetch = null;
+Cypress.Commands.add("persistLogin", () => {
+    Cypress.Cookies.preserveOnce("JSESSIONID");
+});
+
+const stubFetch = win => {
+    delete win.fetch;
+};
+
+Cypress.Commands.add("loadPage", (path = appUrl) => {
+    cy.visit(path, {
+        onBeforeLoad: stubFetch,
+    });
+    cy.get("#app", { log: false, timeout: 20000 }); // Waits for the page to fully load
 });
 
 Cypress.on("uncaught:exception", (err, runnable) => {
     // returning false here prevents Cypress from failing the test
     console.log("uncaught:exception", { err, runnable });
     return false;
+});
+
+Cypress.Commands.add("waitForStep", stepName => {
+    cy.contains(stepName).should($el => {
+        console.log($el);
+        expect($el.attr("class")).to.contain("current-step", `Current step should be ${stepName}`);
+    });
 });
