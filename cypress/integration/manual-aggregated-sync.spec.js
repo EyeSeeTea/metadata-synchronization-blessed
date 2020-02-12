067e13b3e9383a5d5a1969be7fabcc70130224a5
Alexis Rico <sferadev@gmail.com>
2020-02-13 00:39:30 +0100

    Add some basic testing

--- cypress/integration/manual-aggregated-sync.spec.js
+++ cypress/integration/manual-aggregated-sync.spec.js
@@ -11,17 +11,35 @@ context("Manual aggregated sync", function() {
         cy.get(dataTest("page-header-title")).contains("Aggregated Data Synchronization");
     });
 
-    /**
-     * Database: d2-docker-eyeseetea-2-30-datasync-sender
-     */
     it("syncs correctly malaria annual data", function() {
+        // Search in the search-box
         cy.get('[data-test="search"] > div > [aria-invalid="false"]').type("Malaria annual data");
-        cy.get(dataTest("TableCell-data-table-row-0-column-displayname")).contains(
-            "Malaria annual data"
-        );
-        cy.get(dataTest("Checkbox-data-table-row-0")).click();
+
+        // Select one element from the table
+        cy.get(dataTest("TableCell-data-table-row-0-column-displayname"))
+            .contains("Malaria annual data")
+            .click();
+
+        // Open sync dialog
         cy.get(
             '[data-test="objects-table-action-button"] > :nth-child(1) > [focusable="false"]'
         ).click();
+
+        // Select Ghana
+        cy.get(dataTest("DialogContent-aggregated-data-synchronization"))
+            .contains("Ghana")
+            .dblclick();
+
+        // Move to instance selection
+        cy.get(dataTest("DialogContent-aggregated-data-synchronization"))
+            .contains("Instance Selection")
+            .click();
+
+        cy.waitForStep("Instance Selection");
+
+        // TODO: Select receiver instance (multi-selector does not work fine with cypress?)
+
+        // Execute synchronization
+        cy.contains("Synchronize").click();
     });
 });
