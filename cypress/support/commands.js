d7459d7d5c1b2f4b87ee585cf0a609c1c3c3c349
Alexis Rico <sferadev@gmail.com>
2020-02-13 18:53:42 +0100

    Update cypress commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,37 +25,29 @@ const dhis2Auth = _(dhis2AuthEnvValue)
     .fromPairs()
     .value();
 
+Cypress.Cookies.defaults({
+    whitelist: "JSESSIONID",
+});
+
 Cypress.Commands.add("login", (username, _password = null) => {
     const password = _password || dhis2Auth[username];
 
-    cy.log("Login", { username, password });
+    cy.log("Login", { username });
     cy.request({
-        method: "POST",
-        url: `${externalUrl}/dhis-web-commons-security/login.action`,
-        body: {
-            /* eslint-disable @typescript-eslint/camelcase*/
-            j_username: username,
-            j_password: password,
+        method: "GET",
+        url: `${externalUrl}/api/me`,
+        auth: {
+            user: username,
+            pass: password,
         },
-        form: true,
         log: true,
     });
 });
 
-Cypress.Commands.add("persistLogin", () => {
-    Cypress.Cookies.preserveOnce("JSESSIONID");
+Cypress.on("window:before:load", win => {
+    win.fetch = null;
 });
 
-const stubFetch = win => {
-    delete win.fetch;
-};
-
-Cypress.Commands.add("loadPage", (path = appUrl) => {
-    cy.visit(path, {
-        onBeforeLoad: stubFetch,
-    });
-    cy.get("#app", { log: false, timeout: 20000 }); // Waits for the page to fully load
-});
 
 Cypress.on("uncaught:exception", (err, runnable) => {
     // returning false here prevents Cypress from failing the test
@@ -68,4 +60,20 @@ Cypress.Commands.add("waitForStep", stepName => {
         console.log($el);
         expect($el.attr("class")).to.contain("current-step", `Current step should be ${stepName}`);
     });
+});
+
+Cypress.Commands.add("selectInMultiSelector", (selector, option) => {
+    cy.get(selector + " > div select:first").select(option);
+    cy.contains("Selected")
+        .next("button")
+        .click();
+});
+
+Cypress.Commands.add("selectInOrgUnitTree", label => {
+    cy.contains(label)
+        .find("input")
+        .click();
+    cy.contains(label)
+        .should("have.css", "color")
+        .and("equal", "rgb(255, 165, 0)");
 });
