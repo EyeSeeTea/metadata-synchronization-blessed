7cfb1d0a858bf3566414cf61f1248fcd369e2712
idelcano <ignaciodelcano@gmail.com>
2020-02-21 20:48:49 +0100

    added test: not event selected error

--- cypress/integration/manual-event-sync.spec.js
+++ cypress/integration/manual-event-sync.spec.js
@@ -7,11 +7,28 @@ context("Manual event sync", function() {
     const page = new ManualEventSyncPageObject(cy);
 
     const anyOrgUnit = "Ahafo";
+    const anyOrgUnitLevel = "Level 5";
     const anyInstance = "pxPV4coHU56";
     const anyProgram = "ENTO- ";
 
     beforeEach(() => {
         page.open();
+    });
+
+    it("should show the event step error if user try click on next without event", function() {
+        page.search(anyProgram)
+            .selectRow(anyProgram)
+            .openSyncDialog()
+
+            .selectOrgUnit(anyOrgUnit)
+            .selectOrgUnitLevel(anyOrgUnitLevel)
+            .next()
+
+            .selectAllPeriods()
+            .next()
+
+            .next()
+            .error.contains("You need to select at least one event");
     });
 
     it("should have the correct title", function() {
--- cypress/pageobjects/ManualEventSyncPageObject.js
+++ cypress/pageobjects/ManualEventSyncPageObject.js
@@ -15,6 +15,30 @@ class ManualEventSyncPageObject extends ManualSyncPageObject {
         this.cy.get(dataTest("DialogContent-events-synchronization")).selectInOrgUnitTree(orgUnit);
         return this;
     }
+
+    selectOrgUnitLevel(orgUnitLevel) {
+        this.cy
+            .get(
+                '[style="margin-bottom: -24px; margin-top: 0px;"] > [style="position: relative; min-height: 89px;"] > .MuiFormControl-root > .MuiInputBase-root > .MuiSelect-root'
+            )
+            .click();
+        this.cy.get('[data-value="5"]').click();
+
+        this.cy
+            .get(
+                '[style="margin-bottom: -24px; margin-top: 0px;"] > [style="position: relative; min-height: 89px;"] > [style="position: absolute; display: inline-block; top: 24px; margin-left: 16px;"] > [style="position: relative; top: 3px; margin-left: 0px;"] > .MuiButton-label'
+            )
+            .click();
+        return this;
+    }
+
+    selectAllPeriods() {
+        this.cy.get('[data-test="Select-period-dropdown-select"] > [tabindex="0"]').click();
+        this.cy.get('[data-test="MenuItem-period-dropdown-select-element-fixed"]').click();
+        this.cy.get('[data-test="Select-period-dropdown-select"] > [tabindex="0"]').click();
+        this.cy.get('[data-test="MenuItem-period-dropdown-select-element-all"]').click();
+        return this;
+    }
 }
 
 export default ManualEventSyncPageObject;
