7c9c8e4db9c3378a62c857ea0439166bb2df01fc
Jorge Sánchez <xurxosf@gmail.com>
2020-02-21 10:59:41 +0100

    Separate search in own test and comment it by cypress bug

--- cypress/integration/manual-metadata-sync.spec.js
+++ cypress/integration/manual-metadata-sync.spec.js
@@ -3,7 +3,7 @@ import ManualMetadataSyncPageObject from "../pageobjects/ManualMetadataSyncPageO
 context("Manual metadata sync", function() {
     const page = new ManualMetadataSyncPageObject(cy);
 
-    const anyDataElement = "ENTO-IR- Mosquito age";
+    const anyDataElement = "ENTO-ADULT- Household Head Name";
     const anyInstance = "Y5QsHDoD4I0";
 
     beforeEach(() => {
@@ -14,9 +14,14 @@ context("Manual metadata sync", function() {
         page.title.contains("Metadata Synchronization");
     });
 
+    //TODO: this test is commented because some times on this page the search typing not working
+    //      in cypress there are a issue for this bug https://github.com/cypress-io/cypress/issues/5480
+    // it("should show correct rows after search", function() {
+    //     page.search(anyDataElement).selectRow(anyDataElement);
+    // });
+
     it("should syncs correctly one data element", function() {
-        page.search(anyDataElement)
-            .selectRow(anyDataElement)
+        page.selectRow(anyDataElement)
             .openSyncDialog()
             .next()
             .selectReceiverInstance(anyInstance)
@@ -28,8 +33,7 @@ context("Manual metadata sync", function() {
     });
 
     it("should show the instance selection step error if user try click on next without selecting an instance", function() {
-        page.search(anyDataElement)
-            .selectRow(anyDataElement)
+        page.selectRow(anyDataElement)
             .openSyncDialog()
             .next()
             .next()
@@ -37,23 +41,20 @@ context("Manual metadata sync", function() {
     });
 
     it("should have synchronize button disabled to open sync dialog", function() {
-        page.search(anyDataElement)
-            .selectRow(anyDataElement)
+        page.selectRow(anyDataElement)
             .openSyncDialog()
             .syncButton.should("be.disabled");
     });
 
     it("should have synchronize button disabled if only contains default include exclude", function() {
-        page.search(anyDataElement)
-            .selectRow(anyDataElement)
+        page.selectRow(anyDataElement)
             .openSyncDialog()
             .next()
             .syncButton.should("be.disabled");
     });
 
     it("should have synchronize button enabled if contains org unit, periods, category options and one instance", function() {
-        page.search(anyDataElement)
-            .selectRow(anyDataElement)
+        page.selectRow(anyDataElement)
             .openSyncDialog()
             .next()
             .selectReceiverInstance(anyInstance)
