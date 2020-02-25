0ad8de04ad55e733111c61462562fe2a7893905f
Alexis Rico <sferadev@gmail.com>
2020-02-25 08:46:22 +0100

    Switch root model to program instead of program's data elements

--- src/pages/instance-mapping/InstanceMappingPage.tsx
+++ src/pages/instance-mapping/InstanceMappingPage.tsx
@@ -7,7 +7,7 @@ import PageHeader from "../../components/page-header/PageHeader";
 import {
     AggregatedDataElementModel,
     OrganisationUnitModel,
-    ProgramDataElementModel,
+    ProgramModel,
 } from "../../models/d2Model";
 import Instance, { MetadataMappingDictionary } from "../../models/instance";
 import { D2 } from "../../types/d2";
@@ -21,7 +21,7 @@ const config = {
     },
     tracker: {
         title: i18n.t("Tracker metadata mapping"),
-        models: [ProgramDataElementModel],
+        models: [ProgramModel],
     },
     orgUnit: {
         title: i18n.t("Organisation unit metadata mapping"),
