06f8c83c3c764c216c27db258d63b6d231e07e06
Jorge Sánchez <xurxosf@gmail.com>
2020-03-02 09:37:59 +0100

    Create test file for aggregated sync rule edit

---
+++ cypress/integration/aggregated-sync-rule-edit.spec.js
@@ -0,0 +1,47 @@
+import AggregatedSyncRuleDetailPageObject from "../support/page-objects/AggregatedSyncRuleDetailPageObject";
+
+context("Edit Metadata sync rule", function() {
+    const page = new AggregatedSyncRuleDetailPageObject(cy);
+
+    beforeEach(() => {
+        cy.fixture("aggregated-sync-rules.json").then(syncRules => {
+            this.syncRule = syncRules[0];
+            cy.server();
+            cy.route({
+                method: "GET",
+                url: `api/dataStore/metadata-synchronization/rules`,
+                response: syncRules,
+            });
+            page.open(this.syncRule.id);
+        });
+    });
+
+    it("should have the correct title", () => {
+        page.assertTitle(title => title.contains("Edit aggregated synchronization rule"));
+    });
+
+    it("should have the correct general info", () => {
+        page.assertName(name => name.should("have.value", this.syncRule.name))
+            .assertCode(name => name.should("have.value", this.syncRule.code))
+            .assertDescription(name => name.should("have.value", this.syncRule.description));
+    });
+
+    // it("should have the correct selected metadata", () => {
+    //     page.next()
+    //         .checkOnlySelectedItems()
+    //         .assertSelectedMetadata(selectedMetadata => {
+    //             selectedMetadata.contains(
+    //                 `There are ${this.syncRule.builder.metadataIds.length} items selected in all pages.`
+    //             );
+    //         });
+    // });
+
+    // it("should have the correct selected instances", () => {
+    //     page.next()
+    //         .next()
+    //         .next()
+    //         .assertSelectedInstances(selectedInstances =>
+    //             selectedInstances.select(this.syncRule.builder.targetInstances[0])
+    //         );
+    // });
+});
