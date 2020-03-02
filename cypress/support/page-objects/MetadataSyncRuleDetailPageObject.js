d7cec4a7590be8d0c151f8e42c7770445265391c
Alexis Rico <sferadev@gmail.com>
2020-03-02 08:26:13 +0100

    Remove unused import and debugger

--- cypress/support/page-objects/MetadataSyncRuleDetailPageObject.js
+++ cypress/support/page-objects/MetadataSyncRuleDetailPageObject.js
@@ -1,6 +1,5 @@
-import { dataTest } from "../utils";
-import SyncRuleDetailPageObject from "./common/SyncRuleDetailPageObject";
 import * as includeExcludeStep from "../page-utils/includeExcludeStep";
+import SyncRuleDetailPageObject from "./common/SyncRuleDetailPageObject";
 
 class MetadataSyncRuleDetailPageObject extends SyncRuleDetailPageObject {
     constructor(cy) {
