78454fcdbe48bba1874c5cbb7dae005cc4342d0d
Jorge Sánchez <xurxosf@gmail.com>
2020-03-02 08:31:26 +0100

    Create tests for aggregated sync rules

---
+++ cypress/integration/aggregated-sync-rules.spec.js
@@ -0,0 +1,34 @@
+import AggregatedSyncRuleListPageObject from "../support/page-objects/AggregatedSyncRuleListPageObject";
+
+context("Aggregated sync rules", function() {
+    const page = new AggregatedSyncRuleListPageObject(cy);
+
+    beforeEach(() => {
+        const stubApiResponseName = "getRules";
+
+        cy.fixture("aggregated-sync-rules.json").then(syncRules => {
+            this.syncRules = syncRules;
+            cy.server();
+            cy.route({
+                method: "GET",
+                url: `api/dataStore/metadata-synchronization/rules`,
+                response: syncRules,
+            }).as(stubApiResponseName);
+            page.open(stubApiResponseName);
+        });
+    });
+
+    it("should have the correct title", () => {
+        page.assertTitle(title => title.contains("Aggregated Data Synchronization Rules"));
+    });
+
+    it("should contains expected rows count", () => {
+        page.assertRowsCount(this.syncRules.length);
+    });
+
+    it("should sync correctly a sync rule", () => {
+        page.openActionsMenu(0)
+            .synchronize()
+            .assertSyncResultsStatus(status => status.contains("Success"));
+    });
+});
