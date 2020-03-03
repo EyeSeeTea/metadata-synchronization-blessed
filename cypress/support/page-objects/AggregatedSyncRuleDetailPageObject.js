6914919c8b902429e3dd3d7cf96bdca91f65ec43
Jorge Sánchez <xurxosf@gmail.com>
2020-03-03 09:28:08 +0100

    Create tests for aggregated sync rule new page

--- cypress/support/page-objects/AggregatedSyncRuleDetailPageObject.js
+++ cypress/support/page-objects/AggregatedSyncRuleDetailPageObject.js
@@ -2,6 +2,8 @@ import { dataTest } from "../utils";
 import SyncRuleDetailPageObject from "./common/SyncRuleDetailPageObject";
 import * as orgUnitStep from "../page-utils/orgUnitStep";
 import * as dataSetStep from "../page-utils/dataSetStep";
+import * as periodStep from "../page-utils/periodStep";
+import * as categoryOptionsStep from "../page-utils/categoryOptionsStep";
 
 class AggregatedSyncRuleDetailPageObject extends SyncRuleDetailPageObject {
     constructor(cy) {
@@ -25,6 +27,21 @@ class AggregatedSyncRuleDetailPageObject extends SyncRuleDetailPageObject {
 
     assertSelectedDatasetCountMessage(assert) {
         dataSetStep.assertSelectedDatasetCountMessage(assert);
+        return this;
+    }
+
+    selectOrgUnit(orgUnit) {
+        orgUnitStep.selectOrgUnit(".ou-root", orgUnit);
+        return this;
+    }
+
+    selectAllPeriods() {
+        periodStep.selectAllPeriods();
+        return this;
+    }
+
+    selectAllAttributesCategoryOptions() {
+        categoryOptionsStep.selectAllAttributesCategoryOptions();
         return this;
     }
 
