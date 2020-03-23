8b670705dcd5a7399b5166c09ecbb7c478282835
Arnau Sanchez <pyarnau@gmail.com>
2020-03-23 13:21:02 +0100

    Refactor migrations

--- src/migrations/run.ts
+++ src/migrations/run.ts
@@ -1,15 +1,14 @@
-import { Config } from "./types";
 import { D2Api, D2ApiDefault } from "d2-api";
 import _ from "lodash";
 
-import { getDataStore, saveDataStore, dataStoreNamespace } from "../models/dataStore";
+import { Config } from "./types";
+import { promiseMap } from "./utils";
+import { dataStoreNamespace } from "../models/dataStore";
+import { getDataStore, saveDataStore, deleteDataStore } from "../models/dataStore";
 
 import initial from "./tasks/01.init";
 import instancesById from "./tasks/02.instances-by-id";
 import rulesById from "./tasks/03.rules-by-id";
-
-type Migration = { version: number; fn: MigrationFn };
-type MigrationFn = (api: D2Api) => Promise<void>;
 
 const migrations: Migration[] = [
     { version: 1, fn: initial },
@@ -17,70 +16,134 @@ const migrations: Migration[] = [
     { version: 3, fn: rulesById },
 ];
 
+type Migration = { version: number; fn: MigrationFn };
+type MigrationFn = (api: D2Api) => Promise<void>;
 type Debug = (message: string) => void;
 
-type Options = { baseUrl: string; debug?: Debug };
+interface Options {
+    migrations: Migration[];
+    baseUrl: string;
+    debug?: Debug;
+}
 
-async function migrate(migrations: Migration[], options: Options): Promise<void> {
-    const { baseUrl, debug = _.identity } = options;
-    const api = new D2ApiDefault({ baseUrl });
-    const config = await getDataStore<Config>(api, "config", { version: 0 });
-    const migrationsToApply = _(migrations)
-        .filter(info => info.version > config.version)
-        .sortBy(info => info.version)
-        .value();
-    const newVersion = _(migrationsToApply)
-        .map(info => info.version)
-        .max();
+class MigrationRunner {
+    migrations: Migration[];
+    api: D2Api;
+    debug: Debug;
 
-    if (!newVersion) {
-        debug(`No migrations pending to run (current version: ${config.version})`);
-        return;
+    constructor(options: Options) {
+        this.migrations = options.migrations;
+        this.api = new D2ApiDefault({ baseUrl: options.baseUrl });
+        this.debug = options.debug || _.identity;
     }
 
-    backupDataStore(api);
-    const configWithCurrentMigration: Config = { ...config, migration: { version: newVersion } };
-    await saveDataStore(api, "config", configWithCurrentMigration);
-    try {
-        await runMigrations(migrationsToApply, api, debug);
-    } catch (err) {
-        console.error(err);
-        rollbackDataStore(api);
+    async migrate(): Promise<void> {
+        const { api, debug } = this;
+        const config = await getDataStore<Config>(api, "config", { version: 0 });
+        const { newVersion, migrationsToApply } = this.getMigrationData();
+
+        if (!newVersion) {
+            debug(`No migrations pending to run (current version: ${config.version})`);
+            return;
+        }
+
+        debug(`Start migration: version=${newVersion}`);
+        await this.rollBackExistingBackup(config, newVersion);
+        await this.backupDataStore(config, newVersion);
+
+        try {
+            //throw new Error(`cancel: ${migrationsToApply.length}`);
+            await this.runMigrations(migrationsToApply, newVersion);
+        } catch (error) {
+            this.rollbackDataStore(error, config, newVersion);
+        }
+    }
+
+    async runMigrations(migrations: Migration[], newVersion: number) {
+        const { api, debug } = this;
+        for (const migration of migrations) {
+            debug(`Apply migration ${migration.version}: ${migration.fn.name}`);
+            await migration.fn(api);
+        }
+        await saveDataStore(api, "config", { version: newVersion });
+    }
+
+    async rollBackExistingBackup(config: Config, newVersion: number) {
+        await this.rollbackDataStore(new Error("Rollback existing backup"), config, newVersion);
+    }
+
+    async backupDataStore(config: Config, newVersion: number) {
+        const { api, debug } = this;
+        debug(`Start backup`);
+        const allKeys = await this.getDataStoreKeys();
+        const keysToBackup = _(allKeys)
+            .reject(key => key.startsWith("backup-"))
+            .difference(["config"])
+            .compact()
+            .value();
+
         const configWithCurrentMigration: Config = {
             ...config,
-            migration: { version: newVersion, error: err.message || err.toString() },
+            migration: { version: newVersion },
+        };
+        await saveDataStore(api, "config", configWithCurrentMigration);
+
+        await promiseMap(keysToBackup, async key => {
+            const value = await getDataStore(api, key, {});
+            const backupKey = "backup-" + key;
+            debug(`Backup: ${key} -> ${backupKey}`);
+            await saveDataStore(api, backupKey, value);
+        });
+    }
+
+    async getDataStoreKeys(): Promise<string[]> {
+        return this.api
+            .dataStore(dataStoreNamespace)
+            .getKeys()
+            .getData();
+    }
+
+    async rollbackDataStore(error: Error, config: Config, newVersion: number) {
+        const { api, debug } = this;
+        const errorMsg = error.message || error.toString();
+        const allKeys = await this.getDataStoreKeys();
+        const keysToRestore = allKeys.filter(key => key.startsWith("backup-"));
+
+        if (_.isEmpty(keysToRestore)) return;
+
+        debug(`Error: ${errorMsg}`);
+        debug("Start Rollback");
+
+        await promiseMap(keysToRestore, async backupKey => {
+            const value = await getDataStore(api, backupKey, {});
+            const key = backupKey.replace(/^backup-/, "");
+            debug(`Rollback: ${backupKey} -> ${key}`);
+            await saveDataStore(api, key, value);
+            await deleteDataStore(api, backupKey);
+        });
+
+        const configWithCurrentMigration: Config = {
+            ...config,
+            migration: { version: newVersion, error: errorMsg },
         };
         await saveDataStore(api, "config", configWithCurrentMigration);
     }
 
-    await saveDataStore(api, "config", { version: newVersion });
-}
-
-async function runMigrations(migrations: Migration[], api: D2Api, debug: Debug) {
-    for (const migration of migrations) {
-        debug(`Apply migration ${migration.version}: ${migration.fn.name}`);
-        await migration.fn(api);
+    getMigrationData() {
+        const migrationsToApply = _(this.migrations)
+            //.filter(info => info.version > config.version)
+            .sortBy(info => info.version)
+            .value();
+        const newVersion = _(migrationsToApply)
+            .map(info => info.version)
+            .max();
+        return { newVersion, migrationsToApply };
     }
-}
-
-async function backupDataStore(api: D2Api) {
-    // TODO
-    //await saveDataStore(api, "backup", { version: newVersion });
-    const keys = await api
-        .dataStore(dataStoreNamespace)
-        .getKeys()
-        .getData();
-    console.log(keys);
-    process.exit();
-}
-
-async function rollbackDataStore(api: D2Api) {
-    // TODO
-    console.log("TODO", api);
 }
 
 if (require.main === module) {
     const [baseUrl] = process.argv.slice(2);
     if (!baseUrl) throw new Error("Usage: config.ts DHIS2_URL");
-    migrate(migrations, { baseUrl, debug: console.debug });
+    const runner = new MigrationRunner({ migrations, baseUrl, debug: console.debug });
+    runner.migrate();
 }
--- src/migrations/tasks/03.rules-by-id.ts
+++ src/migrations/tasks/03.rules-by-id.ts
@@ -1,15 +1,16 @@
 import _ from "lodash";
-import { D2Api } from "d2-api";
+import { D2Api, Id } from "d2-api";
 
 import { getDataStore, saveDataStore } from "../../models/dataStore";
 import { promiseMap, getDuplicatedIds } from "../utils";
 import { Maybe } from "../../types/utils";
 
-type SynchronizationBuilder = unknown;
+type SynchronizationBuilder = { targetInstances: Id[] };
 
 export interface SynchronizationRuleOld {
     id: string;
     builder: SynchronizationBuilder;
+    targetInstances: Id[];
     [k: string]: unknown;
 }
 
@@ -21,14 +22,19 @@ interface SynchronizationRuleDetailsNew {
 
 export default async function migrate(api: D2Api): Promise<void> {
     const oldRules = await getDataStore<SynchronizationRuleOld[]>(api, "rules", []);
-    const newRules: SynchronizationRuleNew[] = oldRules.map(ins => _.omit(ins, ["builder"]));
+    const newRules: SynchronizationRuleNew[] = oldRules.map(oldRule => ({
+        ..._.omit(oldRule, ["builder"]),
+        targetInstances: oldRule.builder.targetInstances,
+    }));
     const duplicatedIds = getDuplicatedIds(oldRules);
 
     if (duplicatedIds.length > 0)
         throw new Error(`[rules] Duplicate ids: ${duplicatedIds.join(", ")}`);
 
     await promiseMap(oldRules, async oldRule => {
-        const newRuleDetails: Maybe<SynchronizationRuleDetailsNew> = { builder: oldRule.builder };
+        const newRuleDetails: Maybe<SynchronizationRuleDetailsNew> = {
+            builder: oldRule.builder,
+        };
         await saveDataStore(api, "rules-" + oldRule.id, newRuleDetails);
     });
 
