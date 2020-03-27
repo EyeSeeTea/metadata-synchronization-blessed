ee56837ca1075b7b53200a49aafc2c602ef0eb33
idelcano <ignaciodelcano@gmail.com>
2020-03-27 18:25:29 +0100

    added aggregated medium tests

---
+++ cypress/integration/instance-aggregated-mapping.spec.js
@@ -0,0 +1,128 @@
+import MappingAggregatedPageObject from "../support/page-objects/MappingAggregatedPageObject";
+
+context("Aggregated mapping", function() {
+    const page = new MappingAggregatedPageObject(cy);
+
+    const inputs = {
+        instance: "Y5QsHDoD4I0",
+        dataelement: "External funding",
+        alternativeDataelement: "Funding need (USD)",
+    };
+
+    beforeEach(() => {
+        page.open(inputs.instance);
+    });
+
+    it("should set mapping element", function() {
+        page.checkCheckboxByText(inputs.alternativeDataelement);
+
+        page.openSelectedRowMenu().clickOption("Set mapping");
+        page.assertMappedObjectTitle(dialog => dialog.contains("Edit mapping for Funding need (USD) (FRbgzTZ74Hh) - Data Element - Destination instance this instance (8080)"));
+
+        page.selectMappingObject(inputs.alternativeDataelement);
+
+        page.closeDialog();
+        page.assertRowStatus( row => row.contains("Mapped"), inputs.alternativeDataelement);
+    });
+
+    it("should set mapping element and show related metadata mapping option", function() {
+        page.checkCheckboxByText(inputs.dataelement);
+
+        page.openRowMenu().clickOption("Auto-map element");
+
+        page.closeDialog();
+        page.assertRowStatus( row => row.contains("Mapped"), inputs.alternativeDataelement);
+
+        page.checkCheckboxByText(inputs.dataelement);
+
+        page.openSelectedRowMenu().assertOption(option => option.contains("Related metadata mapping"));
+    });
+
+    it("should auto-map element", function() {
+        page.checkCheckboxByText(inputs.dataelement);
+
+        page.openRowMenu().clickOption("Auto-map element");
+        page.assertDialog(dialog => dialog.contains("There are 1 items selected in all pages."));
+        page.assertMappedObjectTitle(dialog => dialog.contains("Edit mapping for External funding (tpz77FcntKx) - Data Element - Destination instance this instance (8080)"));
+
+        page.closeDialog();
+        page.assertRowStatus( row => row.contains("Mapped"), inputs.dataelement);
+    });
+
+    it("should exclude element", function() {
+        page.checkCheckboxByText(inputs.dataelement);
+        page.openRowMenu().clickOption("Exclude mapping");
+        page.assertDialog(dialog => dialog.contains("Are you sure you want to exclude mapping for 1 elements?"));
+        
+        page.clickOkOnDialog();
+        page.assertRowStatus( row => row.contains("Excluded"), inputs.dataelement);
+    });
+
+    it("should validate mapping", function() {
+        page.checkCheckboxByText(inputs.dataelement);
+        page.openRowMenu().clickOption("Auto-map element");
+        page.closeDialog();
+
+        page.openRowMenu().clickOption("Validate mapping");
+        page.assertDialog(dialog => dialog.contains("Are you sure you want to validate mapping for 1 elements?"));
+        
+        page.clickOkOnDialog();
+        page.assertRowStatus( row => row.contains("Mapped"), inputs.dataelement);
+    });
+
+    it("should reset mapping to default values", function() {
+        page.checkCheckboxByText(inputs.dataelement);
+        page.openRowMenu().clickOption("Auto-map element");
+        page.closeDialog();
+
+        page.openRowMenu().clickOption("Reset mapping to default values");
+        page.assertDialog(dialog => dialog.contains("Are you sure you want to reset mapping for 1 elements?"));
+        
+        page.clickOkOnDialog();
+        page.assertRowStatus( row => row.contains("Not mapped"), inputs.dataelement);
+    });
+
+    it("has the correct title", function() {
+        page.assertTitle(title =>
+            title.contains("Aggregated mapping - Destination instance this instance (8080)")
+        );
+    });
+
+    it("has row menu with details action", function() {
+        page.openRowMenu().assertOption(option => option.contains("Details"));
+    });
+
+    it("has row menu with set mapping action", function() {
+        page.openRowMenu().assertOption(option => option.contains("Set mapping"));
+    });
+
+    it("has row menu with auto-map element action", function() {
+        page.openRowMenu().assertOption(option => option.contains("Auto-map element"));
+    });
+
+    it("has row menu with exclude mapping action", function() {
+        page.openRowMenu().assertOption(option => option.contains("Exclude mapping"));
+    });
+
+    it("has row menu with Reset mapping to default values action", function() {
+        page.openRowMenu().assertOption(option =>
+            option.contains("Reset mapping to default values")
+        );
+    });
+
+    it("has general menu with Column settings action", function() {
+        page.openGeneralMenu().assertOption(option => option.contains("Column settings"));
+    });
+
+    it("has general menu with validate mapping action", function() {
+        page.openGeneralMenu().assertOption(option => option.contains("Validate mapping"));
+    });
+
+    it("has general menu with reset mapping action", function() {
+        page.openGeneralMenu().assertOption(option => option.contains("Reset mapping"));
+    });
+
+    it("has general menu with exclude mapping action", function() {
+        page.openGeneralMenu().assertOption(option => option.contains("Exclude mapping"));
+    });
+});
--- cypress/support/page-objects/common/MappingInstancePageObject.js
+++ cypress/support/page-objects/common/MappingInstancePageObject.js
@@ -1,3 +1,4 @@
+import * as mappingStep from "../../page-utils/mappingStep";
 import PageObject from "./PageObject";
 
 export default class MappingInstancePageObject extends PageObject {
@@ -10,7 +11,7 @@ export default class MappingInstancePageObject extends PageObject {
         super.open("/#/instances/mapping/" + instance + "/" + this.key);
     }
 
-    openRowActions() {
+    openRowMenu() {
         this.cy
             .get(
                 ".MuiTableBody-root > :nth-child(1) > .MuiTableCell-alignCenter > .MuiButtonBase-root"
@@ -19,7 +20,12 @@ export default class MappingInstancePageObject extends PageObject {
         return this;
     }
 
-    openBulkActions() {
+    openSelectedRowMenu(text) {
+        this.cy.get(".Mui-selected").rightclick();
+        return this;
+    }
+
+    openGeneralMenu() {
         this.cy
             .get(
                 ".MuiTableHead-root > .MuiTableRow-root > .MuiTableCell-alignCenter > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root"
@@ -30,6 +36,66 @@ export default class MappingInstancePageObject extends PageObject {
 
     assertOption(assert) {
         assert(this.cy.get(".MuiList-root"));
+        return this;
+    }
+
+    clickOption(option) {
+        this.cy
+            .get(".MuiList-root")
+            .contains(option)
+            .click();
+        return this;
+    }
+
+    checkCheckboxByText(text) {
+        this.cy.checkRowCheckboxByText(text);
+        return this;
+    }
+
+    assertDialog(assert) {
+        assert(this.cy.get("div > .MuiDialogContent-root"));
+        return this;
+    }
+
+    clickOkOnDialog() {
+        this.cy
+            .get(".MuiDialogActions-root")
+            .contains("Ok")
+            .click();
+    }
+
+    closeDialog() {
+        this.cy
+            .get(".MuiDialogActions-root")
+            .contains("Close")
+            .parent()
+            .find("Button")
+            .click();
+        return this;
+    }
+
+    assertRowStatus(assert, row) {
+        assert(
+            cy
+                .get("table")
+                .contains(row)
+                .parent()
+        );
+        return this;
+    }
+
+    assertMappedObjectTitle(assert) {
+        assert(cy.get(".MuiDialogTitle-root > .MuiTypography-root"));
+        return this;
+    }
+
+    selectMappingObject(object) {
+        this.cy
+            .get(".MuiDialogContent-root >")
+            .contains(object)
+            .parent()
+            .find("input")
+            .click();
         return this;
     }
 }
