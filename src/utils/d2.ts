78fce84f3d6a948e275e7d42791289c31cfb179a
Jorge Sánchez <xurxosf@gmail.com>
2020-04-24 11:54:11 +0200

    Add todo clean architecture comment for the future

--- src/utils/d2.ts
+++ src/utils/d2.ts
@@ -152,6 +152,10 @@ export function isD2Model(d2: D2, modelName: string): boolean {
 }
 
 export function cleanToModelName(d2: D2, id: string, caller: string): string | null {
+    // TODO: Here we make some transformations from dhis2 children model name in the metadata response
+    // to exepcted children name for the app. On the future when we have the code more refactored
+    // towards clean architecture, this transformations should be realized in the data layer
+    // previously to return response to the sync use case
     if (isD2Model(d2, id)) {
         return d2.models[id].plural;
     } else if (id === "attributeValues") {
