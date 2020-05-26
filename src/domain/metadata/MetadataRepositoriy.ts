f6cb0251cc57e4396ff5cdc7b831409c20f7e32d
Jorge Sánchez <xurxosf@gmail.com>
2020-05-26 10:52:55 +0200

    Add getMetadataByType in MetadataRepository

--- src/domain/metadata/MetadataRepositoriy.ts
+++ src/domain/metadata/MetadataRepositoriy.ts
@@ -1,12 +1,14 @@
 import { MetadataImportResponse, MetadataImportParams } from "./Types";
 import Instance from "../instance/Instance";
 import { Id } from "../common/Entities";
-import { MetadataPackage, MetadataFieldsPackage } from "./entities";
+import { MetadataPackage, MetadataFieldsPackage, MetadataPackageSchema, MetadataEntity } from "./entities";
 
 export interface MetadataRepository {
     getMetadataFieldsByIds<T>(ids: Id[], fields: string): Promise<MetadataFieldsPackage<T>>;
 
     getMetadataByIds(ids: Id[]): Promise<MetadataPackage>;
+
+    getMetadataByType(type: keyof MetadataPackageSchema): Promise<MetadataEntity[]>;
 
     save(metadata: MetadataPackage,
         additionalParams?: MetadataImportParams,
