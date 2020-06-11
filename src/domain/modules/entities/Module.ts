546f41736d9f9defd0695dbe2518e2e215aadc7d
Alexis Rico <sferadev@gmail.com>
2020-06-11 07:36:27 +0200

    Add description to module

--- src/domain/modules/entities/Module.ts
+++ src/domain/modules/entities/Module.ts
@@ -3,6 +3,7 @@ import { SharedObject } from "../../common/entities/SharedObject";
 
 // Represents a sync rule of type module frozen in time
 export interface Module extends SharedObject {
+    description: string;
     metadataIds: string[];
     useDefaultIncludeExclude: boolean;
     metadataIncludeExcludeRules?: MetadataIncludeExcludeRules;
