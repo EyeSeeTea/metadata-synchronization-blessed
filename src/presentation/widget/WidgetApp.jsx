cdf73f5b283fa69b44a116cc700fa0cd9d959e59
Alexis Rico <sferadev@gmail.com>
2020-06-17 08:02:34 +0200

    Add widget app root

--- src/presentation/widget/WidgetApp.jsx
+++ src/presentation/widget/WidgetApp.jsx
@@ -1,6 +1,5 @@
 import { useConfig, useDataQuery } from "@dhis2/app-runtime";
 import i18n from "@dhis2/d2-i18n";
-import { HeaderBar } from "@dhis2/ui-widgets";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { createGenerateClassName, StylesProvider } from "@material-ui/styles";
 import axiosRetry from "axios-retry";
@@ -12,15 +11,12 @@ import React, { useEffect, useState } from "react";
 import { MigrationsRunner } from "../../migrations";
 import Instance from "../../models/instance";
 import { D2Api } from "../../types/d2-api";
-import { initializeAppRoles } from "../../utils/permissions";
 import { AppContext } from "../common/contexts/AppContext";
 import muiThemeLegacy from "../common/themes/dhis2-legacy.theme";
 import { muiTheme } from "../common/themes/dhis2.theme";
 import { CompositionRoot } from "../CompositionRoot";
-import Migrations from "./components/migrations/Migrations";
-import Share from "./components/share/Share";
 import Root from "./pages/Root";
-import "./WebApp.css";
+import "./WidgetApp.css";
 
 const axiosMaxRetries = 3;
 
@@ -33,19 +29,6 @@ const isLangRTL = code => {
     const prefixed = langs.map(c => `${c}-`);
     return _(langs).includes(code) || prefixed.filter(c => code && code.startsWith(c)).length > 0;
 };
-
-function initFeedbackTool(d2, appConfig) {
-    const appKey = _(appConfig).get("appKey");
-
-    if (appConfig && appConfig.feedback) {
-        const feedbackOptions = {
-            ...appConfig.feedback,
-            i18nPath: "feedback-tool/i18n",
-        };
-        if (window.$) window.$.feedbackDhis2(d2, appKey, feedbackOptions);
-        else console.error("Could not initialize feedback tool");
-    }
-}
 
 const configI18n = ({ keyUiLocale: uiLocale }) => {
     i18n.changeLanguage(uiLocale);
@@ -60,8 +43,7 @@ const App = () => {
     const { baseUrl } = useConfig();
     const [appContext, setAppContext] = useState(null);
     const [migrationsState, setMigrationsState] = useState({ type: "checking" });
-    const [showShareButton, setShowShareButton] = useState(false);
-    const { loading, error, data } = useDataQuery(query);
+    const { error, data } = useDataQuery(query);
 
     useEffect(() => {
         const run = async () => {
@@ -75,20 +57,10 @@ const App = () => {
 
             const d2 = await init({ baseUrl: baseUrl + "/api" });
             const api = new D2Api({ baseUrl });
-
             const compositionRoot = new CompositionRoot(api, d2, encryptionKey);
-
-            const appContext = { d2, api, compositionRoot };
-            setAppContext(appContext);
-
-            Object.assign({ d2, api });
+            setAppContext({ d2, api, compositionRoot });
 
             configI18n(data.userSettings);
-            Object.assign(window, { d2, api });
-            setShowShareButton(_(appConfig).get("appearance.showShareButton") || false);
-            initFeedbackTool(d2, appConfig);
-
-            await initializeAppRoles(d2.Api.getApi().baseUrl);
             runMigrations(api).then(setMigrationsState);
         };
 
@@ -96,51 +68,32 @@ const App = () => {
     }, [data, baseUrl]);
 
     if (error) {
-        return (
-            <h3>
-                <a rel="noopener noreferrer" target="_blank" href={baseUrl}>
-                    Login
-                </a>
-                {` ${baseUrl}`}
-            </h3>
-        );
+        return <p>{i18n.t("Unknown DHIS2 error")}</p>;
     } else if (migrationsState.type === "pending") {
         return (
-            <Migrations
-                runner={migrationsState.runner}
-                onFinish={() => setMigrationsState({ type: "checked" })}
-            />
+            <p>{i18n.t("Widget cannot be used until an administrator opens the application")}</p>
         );
-    } else if (
-        loading ||
-        !appContext?.d2 ||
-        !appContext?.api ||
-        migrationsState.type === "checking"
-    ) {
-        return <h3>Connecting to {baseUrl}...</h3>;
-    } else if (migrationsState.type === "checked") {
-        return (
-            <StylesProvider generateClassName={generateClassName}>
-                <MuiThemeProvider theme={muiTheme}>
-                    <OldMuiThemeProvider muiTheme={muiThemeLegacy}>
-                        <LoadingProvider>
-                            <SnackbarProvider>
-                                <HeaderBar appName={i18n.t("MetaData Synchronization")} />
+    }
 
+    return (
+        <StylesProvider generateClassName={generateClassName}>
+            <MuiThemeProvider theme={muiTheme}>
+                <OldMuiThemeProvider muiTheme={muiThemeLegacy}>
+                    <LoadingProvider>
+                        <SnackbarProvider>
+                            {!!appContext && (
                                 <div id="app" className="content">
                                     <AppContext.Provider value={appContext}>
                                         <Root />
                                     </AppContext.Provider>
                                 </div>
-
-                                <Share visible={showShareButton} />
-                            </SnackbarProvider>
-                        </LoadingProvider>
-                    </OldMuiThemeProvider>
-                </MuiThemeProvider>
-            </StylesProvider>
-        );
-    }
+                            )}
+                        </SnackbarProvider>
+                    </LoadingProvider>
+                </OldMuiThemeProvider>
+            </MuiThemeProvider>
+        </StylesProvider>
+    );
 };
 
 async function runMigrations(api) {
---
+++ src/presentation/widget/pages/Root.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Root() {
+    return <p>Hello World!</p>;
+}
+
+export default Root;
