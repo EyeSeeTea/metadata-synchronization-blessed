c5e82749938aa706ff98f47435c5671a72909125
Alexis Rico <sferadev@gmail.com>
2020-07-15 10:20:26 +0200

    Hide responsible column on local instance

--- src/presentation/webapp/components/metadata-table/MetadataTable.tsx
+++ src/presentation/webapp/components/metadata-table/MetadataTable.tsx
@@ -507,6 +507,7 @@ const MetadataTable: React.FC<MetadataTableProps> = ({
             ? {
                   name: "responsible",
                   text: i18n.t("Responsible"),
+                  hidden: !remoteInstance,
                   getValue: (row: MetadataType) => {
                       const { userAccesses = [], userGroupAccesses = [] } =
                           responsibles.find(({ id }) => row.id === id) ?? {};
