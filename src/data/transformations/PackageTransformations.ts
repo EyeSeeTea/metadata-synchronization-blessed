391003179f4f6d74f92766eacf37db15896388d0
Alexis Rico <sferadev@gmail.com>
2020-08-04 12:50:39 +0200

    Example transformation for report tables on API34

--- src/data/transformations/PackageTransformations.ts
+++ src/data/transformations/PackageTransformations.ts
@@ -1,6 +1,63 @@
 import { Transformation } from "../../domain/transformations/entities/Transformation";
 
-export const metadataTransformations: Transformation[] = [];
+export const metadataTransformations: Transformation[] = [
+    {
+        name: "report-table-params",
+        apiVersion: 34,
+        apply: ({ reports, ...rest }: any) => {
+            return {
+                ...rest,
+                reports: reports?.map(({ reportTable, reportParams = {}, ...rest }: any) => {
+                    const {
+                        paramGrandParentOrganisationUnit: grandParentOrganisationUnit,
+                        paramParentOrganisationUnit: parentOrganisationUnit,
+                        paramReportingPeriod: reportingPeriod,
+                        paramOrganisationUnit: organisationUnit,
+                        ...restReportParams
+                    } = reportParams;
+
+                    return {
+                        ...rest,
+                        visualization: reportTable,
+                        reportParams: {
+                            ...restReportParams,
+                            grandParentOrganisationUnit,
+                            parentOrganisationUnit,
+                            reportingPeriod,
+                            organisationUnit,
+                        },
+                    };
+                }),
+            };
+        },
+        undo: ({ reports, ...rest }: any) => {
+            return {
+                ...rest,
+                reports: reports?.map(({ visualization, reportParams = {}, ...rest }: any) => {
+                    const {
+                        grandParentOrganisationUnit: paramGrandParentOrganisationUnit,
+                        parentOrganisationUnit: paramParentOrganisationUnit,
+                        reportingPeriod: paramReportingPeriod,
+                        organisationUnit: paramOrganisationUnit,
+                        ...restReportParams
+                    } = reportParams;
+
+                    return {
+                        ...rest,
+                        reportTable: visualization,
+                        reportParams: {
+                            ...restReportParams,
+                            paramGrandParentOrganisationUnit,
+                            paramParentOrganisationUnit,
+                            paramReportingPeriod,
+                            paramOrganisationUnit,
+                        },
+                    };
+                }),
+            };
+        },
+    },
+];
 
 export const aggregatedTransformations: Transformation[] = [];
 
