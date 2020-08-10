e9ceba98fe4de3a3320e2b11257c68097f22c152
Alexis Rico <sferadev@gmail.com>
2020-08-10 12:00:41 +0200

    Allow editing module by id

--- src/presentation/webapp/pages/modules-creation/ModuleCreationPage.tsx
+++ src/presentation/webapp/pages/modules-creation/ModuleCreationPage.tsx
@@ -1,19 +1,22 @@
 import { ConfirmationDialog } from "d2-ui-components";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useLocation, useParams } from "react-router-dom";
 import { Module } from "../../../../domain/modules/entities/Module";
 import i18n from "../../../../locales";
+import { useAppContext } from "../../../common/contexts/AppContext";
 import { ModuleWizard } from "../../components/module-wizard/ModuleWizard";
 import PageHeader from "../../components/page-header/PageHeader";
 
 const ModuleCreationPage: React.FC = () => {
+    const { compositionRoot } = useAppContext();
     const history = useHistory();
-    const { action } = useParams<{ action: "edit" | "new" }>();
+    const { id, action } = useParams<{ id: string; action: "edit" | "new" }>();
     const location = useLocation<{ module?: Module }>();
 
     const [dialogOpen, updateDialogOpen] = useState(false);
+    const [module, updateModule] = useState(() => location.state?.module);
 
-    const isEdit = action === "edit" && !!location.state?.module;
+    const isEdit = action === "edit" && (!!module || !!id);
     const title = !isEdit ? i18n.t(`New module`) : i18n.t(`Edit module`);
     const cancel = !isEdit ? i18n.t(`Cancel module creation`) : i18n.t(`Cancel module editing`);
 
@@ -24,6 +27,10 @@ const ModuleCreationPage: React.FC = () => {
         updateDialogOpen(false);
         history.push(`/modules`);
     };
+
+    useEffect(() => {
+        if (!module && !!id) compositionRoot.modules.get(id).then(updateModule);
+    }, [compositionRoot, module, id]);
 
     return (
         <React.Fragment>
@@ -38,11 +45,14 @@ const ModuleCreationPage: React.FC = () => {
 
             <PageHeader title={title} onBackClick={openDialog} />
 
-            <ModuleWizard
-                onCancel={openDialog}
-                onClose={onClose}
-                editModule={location.state?.module}
-            />
+            {module && (
+                <ModuleWizard
+                    onCancel={openDialog}
+                    onClose={onClose}
+                    module={module}
+                    onChange={updateModule}
+                />
+            )}
         </React.Fragment>
     );
 };
