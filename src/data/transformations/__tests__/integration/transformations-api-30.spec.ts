b482219c0f8cce3859e13eb167dc67616050687c
idelcano <ignaciodelcano@gmail.com>
2020-08-13 18:13:55 +0200

    Added featureType to captureCoordinates program transformation api 31 to 30 more tests

--- src/data/transformations/__tests__/integration/transformations-api-30.spec.ts
+++ src/data/transformations/__tests__/integration/transformations-api-30.spec.ts
@@ -39,6 +39,16 @@ describe("Sync metadata", () => {
                     name: "Test tracker program",
                     featureType: "POINT",
                 },
+                {
+                    id: "id2",
+                    name: "Test tracker program",
+                    featureType: "POLYGON",
+                },
+                {
+                    id: "id3",
+                    name: "Test tracker program",
+                    featureType: "NONE",
+                },
             ],
         }));
 
@@ -110,6 +120,82 @@ describe("Sync metadata", () => {
 
         // Assert old properties are not anymore
         expect(response.programs[0].captureCoordinates).toEqual(true);
+
+        // Assert we have not updated local metadata
+        expect(local.db.metadata.find(1)).toBeNull();
+    });
+
+    it("Local server to remote - program featureType POLYGON to captureCoordinates - API 31 to API 30", async () => {
+        const localInstance = Instance.build({
+            url: "http://origin.test",
+            name: "Testing",
+            version: "2.31",
+        });
+
+        const builder: SynchronizationBuilder = {
+            originInstance: "LOCAL",
+            targetInstances: ["DESTINATION"],
+            metadataIds: ["id1", "id2", "id3"],
+            excludedIds: [],
+        };
+
+        const useCase = new MetadataSyncUseCase(builder, repositoryFactory, localInstance, "");
+
+        const payload = await useCase.buildPayload();
+        expect(payload.programs?.find(({ id }) => id === "id2")).toBeDefined();
+
+        for await (const { done } of useCase.execute()) {
+            if (done) console.log("Done");
+        }
+
+        // Assert object has been created on remote
+        const response = remote.db.metadata.find(1);
+        expect(response.programs[1].id).toEqual("id2");
+        expect(response.programs[1].name).toEqual("Test tracker program");
+
+        // Assert new properties have the correct values
+        expect(response.programs[1].featureType).toEqual("POLYGON");
+
+        // Assert old properties are not anymore
+        expect(response.programs[1].captureCoordinates).toEqual(true);
+
+        // Assert we have not updated local metadata
+        expect(local.db.metadata.find(1)).toBeNull();
+    });
+
+    it("Local server to remote - program featureType POLYGON to captureCoordinates - API 31 to API 30", async () => {
+        const localInstance = Instance.build({
+            url: "http://origin.test",
+            name: "Testing",
+            version: "2.31",
+        });
+
+        const builder: SynchronizationBuilder = {
+            originInstance: "LOCAL",
+            targetInstances: ["DESTINATION"],
+            metadataIds: ["id3"],
+            excludedIds: [],
+        };
+
+        const useCase = new MetadataSyncUseCase(builder, repositoryFactory, localInstance, "");
+
+        const payload = await useCase.buildPayload();
+        expect(payload.programs?.find(({ id }) => id === "id3")).toBeDefined();
+
+        for await (const { done } of useCase.execute()) {
+            if (done) console.log("Done");
+        }
+
+        // Assert object has been created on remote
+        const response = remote.db.metadata.find(1);
+        expect(response.programs[2].id).toEqual("id3");
+        expect(response.programs[2].name).toEqual("Test tracker program");
+
+        // Assert new properties have the correct values
+        expect(response.programs[2].featureType).toEqual("NONE");
+
+        // Assert old properties are not anymore
+        expect(response.programs[2].captureCoordinates).toEqual(false);
 
         // Assert we have not updated local metadata
         expect(local.db.metadata.find(1)).toBeNull();
