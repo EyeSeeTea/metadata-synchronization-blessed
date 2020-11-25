dbcd838029e7b13678d4354abdf797c6e8aad63b
Alexis Rico <sferadev@gmail.com>
2020-11-25 12:08:48 +0100

    Add basic integration test

---
+++ src/data/metadata/__tests__/integration/local-instance-mapped.spec.ts
@@ -0,0 +1,181 @@
+import { Request, Server } from "miragejs";
+import { AnyRegistry } from "miragejs/-types";
+import Schema from "miragejs/orm/schema";
+import { AggregatedSyncUseCase } from "../../../../domain/aggregated/usecases/AggregatedSyncUseCase";
+import {
+    Repositories,
+    RepositoryFactory,
+} from "../../../../domain/common/factories/RepositoryFactory";
+import { Instance } from "../../../../domain/instance/entities/Instance";
+import { SynchronizationBuilder } from "../../../../types/synchronization";
+import { startDhis } from "../../../../utils/dhisServer";
+import { AggregatedD2ApiRepository } from "../../../aggregated/AggregatedD2ApiRepository";
+import { InstanceD2ApiRepository } from "../../../instance/InstanceD2ApiRepository";
+import { StorageDataStoreClient } from "../../../storage/StorageDataStoreClient";
+import { TransformationD2ApiRepository } from "../../../transformations/TransformationD2ApiRepository";
+import { MetadataD2ApiRepository } from "../../MetadataD2ApiRepository";
+
+const repositoryFactory = buildRepositoryFactory();
+
+describe("Sync metadata", () => {
+    let local: Server;
+
+    beforeAll(() => {
+        jest.setTimeout(30000);
+    });
+
+    beforeEach(() => {
+        local = startDhis({ urlPrefix: "http://origin.test" });
+
+        local.get("/categoryOptionCombos", async () => ({
+            categoryOptionCombos: [
+                {
+                    name: "default",
+                    id: "default8",
+                    categoryCombo: { id: "default7" },
+                    categoryOptions: [{ id: "default5" }],
+                },
+            ],
+        }));
+
+        local.get("/metadata", async (_schema, request) => {
+            if (request.queryParams.filter === "id:in:[dataSet1]")
+                return {
+                    dataSets: [
+                        {
+                            name: "Test data set",
+                            id: "dataSet1",
+                            dataSetElements: [
+                                {
+                                    dataElement: {
+                                        name: "Test data element 1",
+                                        id: "id1",
+                                    },
+                                    dataSet: {
+                                        id: "dataSet1",
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                };
+
+            if (request.queryParams.filter === "code:eq:default")
+                return {
+                    categoryOptions: [{ id: "default1" }],
+                    categories: [{ id: "default2" }],
+                    categoryCombos: [{ id: "default3" }],
+                    categoryOptionCombos: [{ id: "default4" }],
+                };
+
+            console.log("Unknown metadata request", request.queryParams);
+        });
+
+        local.get("/dataValueSets", async () => ({
+            dataValues: [
+                {
+                    dataElement: "id1",
+                    period: "20191231",
+                    orgUnit: "Global",
+                    categoryOptionCombo: "default4",
+                    attributeOptionCombo: "default4",
+                    value: "test-value-1",
+                    storedBy: "test-user",
+                    created: "2020-05-28T08:32:53.000+0000",
+                    lastUpdated: "2020-05-28T08:32:53.000+0000",
+                    followup: false,
+                },
+            ],
+        }));
+
+        local.get("/dataStore/metadata-synchronization/instances", async () => [
+            {
+                id: "LOCAL",
+                name: "This instance",
+                url: "http://origin.test",
+                description: "",
+            },
+        ]);
+
+        local.get("/dataStore/metadata-synchronization/instances-LOCAL", async () => ({
+            metadataMapping: {
+                aggregatedDataElements: {
+                    id1: {
+                        mappedId: "id2",
+                        mappedName: "foo",
+                        code: "foo",
+                        conflicts: false,
+                        global: false,
+                        mapping: {},
+                    },
+                },
+            },
+        }));
+
+        const addAggregatedToDb = async (schema: Schema<AnyRegistry>, request: Request) => {
+            schema.db.dataValueSets.insert(JSON.parse(request.requestBody));
+
+            return {
+                responseType: "ImportSummary",
+                status: "WARNING",
+                description: "Import process completed successfully",
+                importCount: { imported: 0, updated: 0, ignored: 477, deleted: 0 },
+                conflicts: [
+                    {
+                        object: "id1",
+                        value: "Data element not found or not accessible",
+                    },
+                ],
+                dataSetComplete: "false",
+            };
+        };
+
+        local.db.createCollection("dataValueSets", []);
+        local.post("/dataValueSets", addAggregatedToDb);
+    });
+
+    afterEach(() => {
+        local.shutdown();
+    });
+
+    it("Local server to local - same version", async () => {
+        const localInstance = Instance.build({
+            url: "http://origin.test",
+            name: "Testing",
+            version: "2.30",
+        });
+
+        const builder: SynchronizationBuilder = {
+            originInstance: "LOCAL",
+            targetInstances: ["LOCAL"],
+            metadataIds: ["dataSet1"],
+            excludedIds: [],
+        };
+
+        const sync = new AggregatedSyncUseCase(builder, repositoryFactory, localInstance, "");
+
+        const payload = await sync.buildPayload();
+        expect(payload.dataValues?.find(({ value }) => value === "test-value-1")).toBeDefined();
+
+        for await (const _sync of sync.execute()) {
+            // no-op
+        }
+
+        const response = local.db.dataValueSets.find(1);
+        expect(response.dataValues[0].value).toEqual("test-value-1");
+        expect(response.dataValues[0].dataElement).toEqual("id2");
+    });
+
+});
+
+function buildRepositoryFactory() {
+    const repositoryFactory: RepositoryFactory = new RepositoryFactory("");
+    repositoryFactory.bind(Repositories.InstanceRepository, InstanceD2ApiRepository);
+    repositoryFactory.bind(Repositories.StorageRepository, StorageDataStoreClient);
+    repositoryFactory.bind(Repositories.MetadataRepository, MetadataD2ApiRepository);
+    repositoryFactory.bind(Repositories.AggregatedRepository, AggregatedD2ApiRepository);
+    repositoryFactory.bind(Repositories.TransformationRepository, TransformationD2ApiRepository);
+    return repositoryFactory;
+}
+
+export {};
