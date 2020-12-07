ed336e311bb439010440ee5dcd1949530ef07719
Jorge Sánchez <xurxosf@gmail.com>
2020-12-07 08:38:54 +0100

    Center MSFSettingsDialog content

--- src/presentation/react/msf-aggregate-data/components/msf-Settings/MSFSettingsDialog.tsx
+++ src/presentation/react/msf-aggregate-data/components/msf-Settings/MSFSettingsDialog.tsx
@@ -1,3 +1,4 @@
+import { Box, makeStyles } from "@material-ui/core";
 import { ConfirmationDialog } from "d2-ui-components";
 import React, { useMemo, useState } from "react";
 import i18n from "../../../../../locales";
@@ -21,6 +22,7 @@ export const MSFSettingsDialog: React.FC<MSFSettingsDialogProps> = ({
     msfSettings,
 }) => {
     const [useSyncRule, setUseSyncRule] = useState(msfSettings.runAnalytics.toString());
+    const classes = useStyles();
 
     const useSyncRuleItems = useMemo(() => {
         return [
@@ -63,13 +65,21 @@ export const MSFSettingsDialog: React.FC<MSFSettingsDialogProps> = ({
             cancelText={i18n.t("Cancel")}
             saveText={i18n.t("Save")}
         >
-            <Dropdown
-                label={i18n.t("Run Analytics")}
-                items={useSyncRuleItems}
-                onValueChange={setUseSyncRule}
-                value={useSyncRule}
-                hideEmpty
-            />
+            <Box className={classes.root} width="80%">
+                <Dropdown
+                    label={i18n.t("Run Analytics")}
+                    items={useSyncRuleItems}
+                    onValueChange={setUseSyncRule}
+                    value={useSyncRule}
+                    hideEmpty
+                />
+            </Box>
         </ConfirmationDialog>
     );
 };
+
+const useStyles = makeStyles(() => ({
+    root: {
+        margin: "0 auto",
+    },
+}));
