6172a95b76cd6bd9dc9f3849200ee94761dee547
Sergio <sergio@tinkerista.com>
2020-12-08 02:34:36 +0100

    mdsync web updated links

--- metadatasync/lib/main.js
+++ metadatasync/lib/main.js
@@ -1,5 +1,46 @@
 'use strict';
-
+//goToPageLanguageOnLoad()
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('Hello Bulma!');
+  //console.log('Hello Bulma!');
 });
+
+
+function goToPageLanguageOnLoad() {
+  const savedLang = getCookie('lang')
+  console.log(savedLang)
+  if (savedLang.length === 2) changeLanguage(savedLang);
+}
+
+
+function changeLanguage(code) {
+  const basepath = "/metadatasync";
+  if (code === 'es') location.pathname = basepath+"/index_es.html";
+  else if (code === "fr") location.pathname = basepath+"/index_fr.html";
+  else location.pathname = basepath+"/index.html";
+ setCookie('lang',code,365)
+
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
