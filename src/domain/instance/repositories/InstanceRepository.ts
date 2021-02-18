dfb4386e6774d16e333eec9d85e4f3885ac729c7
Jorge Sánchez <xurxosf@gmail.com>
2021-02-18 09:26:36 +0100

    Add getById method to InstanceRepository

--- src/domain/instance/repositories/InstanceRepository.ts
+++ src/domain/instance/repositories/InstanceRepository.ts
@@ -1,14 +1,16 @@
 import { D2Api } from "../../../types/d2-api";
+import { ConfigRepository } from "../../config/repositories/ConfigRepository";
 import { OrganisationUnit } from "../../metadata/entities/MetadataEntities";
 import { Instance } from "../entities/Instance";
 import { InstanceMessage } from "../entities/Message";
 import { User } from "../entities/User";
 
 export interface InstanceRepositoryConstructor {
-    new (instance: Instance, encryptionKey: string): InstanceRepository;
+    new(configRepository: ConfigRepository, instance: Instance, encryptionKey: string): InstanceRepository;
 }
 
 export interface InstanceRepository {
+    getById(id: string): Promise<Instance | undefined>
     getApi(): D2Api;
     getBaseUrl(): string;
     getUser(): Promise<User>;
