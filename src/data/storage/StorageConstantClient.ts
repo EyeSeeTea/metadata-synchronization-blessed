acf64f8b8a476303253b31402d7adbd07eda847a
Alexis Rico <sferadev@gmail.com>
2021-03-10 08:44:14 +0100

    Split constant storage into key-value

--- src/data/storage/StorageConstantClient.ts
+++ src/data/storage/StorageConstantClient.ts
@@ -1,12 +1,14 @@
 import { generateUid } from "d2/uid";
+import _ from "lodash";
 import { Instance } from "../../domain/instance/entities/Instance";
 import { ObjectSharing, StorageClient } from "../../domain/storage/repositories/StorageClient";
 import { D2Api } from "../../types/d2-api";
 import { Dictionary } from "../../types/utils";
+import { promiseMap } from "../../utils/common";
 import { getD2APiFromInstance } from "../../utils/d2-utils";
 
-const defaultName = "MDSync Storage";
-const defaultKey = "MDSYNC_STORAGE";
+const CONSTANT_NAME = "MDSync Storage";
+const CONSTANT_PREFIX = "MDSYNC_STORAGE_";
 
 export class StorageConstantClient extends StorageClient {
     public type = "constant" as const;
@@ -19,59 +21,70 @@ export class StorageConstantClient extends StorageClient {
     }
 
     public async getObject<T extends object>(key: string): Promise<T | undefined> {
-        const { value } = await this.getConstant<Dictionary<T>>();
-        return value ? value[key] : undefined;
+        const { value } = await this.getConstant<T>(key);
+        return value;
     }
 
     public async getOrCreateObject<T extends object>(key: string, defaultValue: T): Promise<T> {
-        const { id, value = {} } = await this.getConstant<Dictionary<T>>();
-        if (!value[key]) {
-            await this.updateConstant(id, { ...value, [key]: defaultValue });
-        }
-        return value[key] ?? defaultValue;
+        const { id, value } = await this.getConstant<T>(key);
+        if (!value) await this.updateConstant(id, key, defaultValue);
+        return value ?? defaultValue;
     }
 
     public async saveObject<T extends object>(key: string, keyValue: T): Promise<void> {
-        const { id, value = {} } = await this.getConstant<Dictionary<T>>();
-        await this.updateConstant(id, { ...value, [key]: keyValue });
+        const { id } = await this.getConstant<T>(key);
+        await this.updateConstant(id, key, keyValue);
     }
 
     public async removeObject(key: string): Promise<void> {
-        const { id, value = {} } = await this.getConstant<Dictionary<unknown>>();
-        await this.updateConstant(id, { ...value, [key]: undefined });
+        const { id } = await this.getConstant<unknown>(key);
+        if (id) await this.api.models.constants.delete({ id }).getData();
     }
 
     public async clearStorage(): Promise<void> {
         try {
-            const { objects: constants } = await this.api.models.constants
+            const { objects } = await this.api.models.constants
                 .get({
                     paging: false,
                     fields: { id: true, code: true, name: true, description: true },
-                    filter: { code: { eq: defaultKey } },
+                    filter: { code: { $like: CONSTANT_PREFIX } },
                 })
                 .getData();
 
-            const { id } = constants[0] ?? {};
-
-            if (id) await this.api.models.constants.delete({ id }).getData();
+            await promiseMap(objects, ({ id }) =>
+                this.api.models.constants.delete({ id }).getData()
+            );
         } catch (error) {
             console.log(error);
         }
     }
 
     public async clone(): Promise<Dictionary<unknown>> {
-        const { value = {} } = await this.getConstant<Dictionary<unknown>>();
-        return value;
+        const constants = await this.lookupConstants();
+
+        return _(constants)
+            .map(({ code, description }) => [
+                code.replace(new RegExp(`^${CONSTANT_PREFIX}`, ""), ""),
+                JSON.parse(description),
+            ])
+            .fromPairs()
+            .value();
     }
 
-    public async import(value: Dictionary<unknown>): Promise<void> {
-        const { id } = await this.getConstant();
-        await this.updateConstant(id, value);
+    public async import(dump: Dictionary<unknown>): Promise<void> {
+        const pairs = _.toPairs(dump);
+
+        await promiseMap(pairs, async ([key, value]) => {
+            await this.saveObject(key, value as object);
+        });
     }
 
     public async listKeys(): Promise<string[]> {
-        const { value = {} } = await this.getConstant<Dictionary<unknown>>();
-        return Object.keys(value);
+        const constants = await this.lookupConstants();
+
+        return _(constants)
+            .map(({ code }) => code.replace(new RegExp(`^${CONSTANT_PREFIX}`, ""), ""))
+            .value();
     }
 
     public getObjectSharing(_key: string): Promise<ObjectSharing | undefined> {
@@ -82,14 +95,18 @@ export class StorageConstantClient extends StorageClient {
         throw new Error("Method not implemented.");
     }
 
-    private async updateConstant<T extends object>(id: string, value: T): Promise<void> {
+    private async updateConstant<T extends object>(
+        id: string,
+        key: string,
+        value: T
+    ): Promise<void> {
         await this.api.metadata
             .post({
                 constants: [
                     {
                         id,
-                        code: defaultKey,
-                        name: defaultName,
+                        code: formatKey(key),
+                        name: formatName(key),
                         description: JSON.stringify(value, null, 2),
                         value: 1,
                     },
@@ -98,24 +115,50 @@ export class StorageConstantClient extends StorageClient {
             .getData();
     }
 
-    private async getConstant<T>(): Promise<{ id: string; value?: T }> {
+    private async getConstant<T>(key: string): Promise<{ id: string; code?: string; value?: T }> {
         const { objects: constants } = await this.api.models.constants
             .get({
                 paging: false,
                 fields: { id: true, code: true, name: true, description: true },
-                filter: { code: { eq: defaultKey } },
+                filter: { code: { eq: formatKey(key) } },
             })
             .getData();
 
-        const { id = generateUid(), description } = constants[0] ?? {};
+        const { id = generateUid(), code, description } = constants[0] ?? {};
 
         try {
             const value = description ? JSON.parse(description) : undefined;
-            return { id, value };
+            return { id, code, value };
         } catch (error) {
             console.error(error);
+            return { id, code };
         }
-
-        return { id };
     }
+
+    private async lookupConstants(): Promise<Constant[]> {
+        const { objects } = await this.api.models.constants
+            .get({
+                paging: false,
+                fields: { id: true, code: true, name: true, description: true },
+                filter: { code: { $like: CONSTANT_PREFIX } },
+            })
+            .getData();
+
+        return objects;
+    }
+}
+
+function formatKey(key: string): string {
+    return _.toUpper(_.snakeCase(`${CONSTANT_PREFIX}_${key}`));
+}
+
+function formatName(name: string): string {
+    return _.startCase(`${CONSTANT_NAME} - ${name}`);
+}
+
+interface Constant {
+    id: string;
+    code: string;
+    name: string;
+    description: string;
 }
