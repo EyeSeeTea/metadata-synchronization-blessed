4fd00cd475e1a50637f34e7667e5dd486c7f320f
Jorge Sánchez <xurxosf@gmail.com>
2021-03-22 16:50:35 +0100

    Map program and orgUnit for TEI

--- src/domain/tracked-entity-instances/mapper/TEIsPayloadMapper.ts
+++ src/domain/tracked-entity-instances/mapper/TEIsPayloadMapper.ts
@@ -1,6 +1,7 @@
 import { MetadataMappingDictionary } from "../../mapping/entities/MetadataMapping";
 import { SynchronizationPayload } from "../../synchronization/entities/SynchronizationPayload";
 import { PayloadMapper } from "../../synchronization/mapper/PayloadMapper";
+import { cleanOrgUnitPath } from "../../synchronization/utils";
 import { TEIsPackage } from "../entities/TEIsPackage";
 import { TrackedEntityInstance } from "../entities/TrackedEntityInstance";
 
@@ -11,10 +12,41 @@ export class TEIsPayloadMapper implements PayloadMapper {
         const teiPackage = payload as TEIsPackage;
 
         const trackedEntityInstances = teiPackage.trackedEntityInstances.map(tei => {
-            const { relationshipTypes = {} } = this.mapping;
+            const {
+                relationshipTypes = {},
+                organisationUnits = {},
+                trackerPrograms = {},
+            } = this.mapping;
+
+            const mappedOrgUnit = organisationUnits[tei.orgUnit]?.mappedId ?? tei.orgUnit;
 
             return {
                 ...tei,
+                orgUnit: cleanOrgUnitPath(mappedOrgUnit),
+                programOwners: tei.programOwners.map(owner => {
+                    const mappedOrgUnit =
+                        organisationUnits[owner.ownerOrgUnit]?.mappedId ?? owner.ownerOrgUnit;
+
+                    const mappedProgram = trackerPrograms[owner.program]?.mappedId ?? owner.program;
+
+                    return {
+                        ...owner,
+                        ownerOrgUnit: cleanOrgUnitPath(mappedOrgUnit),
+                        program: mappedProgram,
+                    };
+                }),
+                enrollments: tei.enrollments.map(enrollment => {
+                    const mappedOrgUnit =
+                        organisationUnits[enrollment.orgUnit]?.mappedId ?? enrollment.orgUnit;
+                    const mappedProgram =
+                        trackerPrograms[enrollment.program]?.mappedId ?? enrollment.program;
+
+                    return {
+                        ...enrollment,
+                        orgUnit: cleanOrgUnitPath(mappedOrgUnit),
+                        program: mappedProgram,
+                    };
+                }),
                 relationships: tei.relationships.map(rel => {
                     const relationshipTypeId =
                         relationshipTypes[rel.relationshipType]?.mappedId ?? rel.relationshipType;
--- src/domain/tracked-entity-instances/mapper/__tests__/TEIsPayloadMapper.spec.ts
+++ src/domain/tracked-entity-instances/mapper/__tests__/TEIsPayloadMapper.spec.ts
@@ -3,9 +3,15 @@ import { TEIsPayloadMapper } from "../TEIsPayloadMapper";
 
 import singleTEI from "./data/tei/singleTEI.json";
 import twoRelatedTEIs from "./data/tei/twoRelatedTEIs.json";
+
+import mappingWithRelationshipTypes from "./data/mapping/mappingWithRelationshipTypes.json";
+import mappingWithOrgUnits from "./data/mapping/mappingWithOrgUnits.json";
+import mappingTrackerProgramToTrackerProgram from "./data/mapping/mappingTrackerProgramToTrackerProgram.json";
+
 import twoRelatedTEIsMapWithRelationship from "./data/expected/twoRelatedTEIsMapWithRelationship.json";
 import singleTEIMapWithRelationship from "./data/expected/singleTEIMapWithRelationship.json";
-import mappingWithRelationshipTypes from "./data/mapping/mappingWithRelationshipTypes.json";
+import singleTEIMapWithOrgUnit from "./data/expected/singleTEIMapWithOrgUnit.json";
+import singleTEIMapWithProgram from "./data/expected/singleTEIMapWithProgram.json";
 
 describe("TEIsPayloadMapper", () => {
     it("should return the same payload if mapping is empty", async () => {
@@ -26,7 +32,7 @@ describe("TEIsPayloadMapper", () => {
 
         expect(mappedPayload).toEqual(singleTEIMapWithRelationship);
     });
-    it("should remove duplicate relationships to avoid alreay exists relationships 409 api bug", async () => {
+    it("should remove duplicate relationships to avoid already exists relationships 409 api bug", async () => {
         const teiMapper = new TEIsPayloadMapper(mappingWithRelationshipTypes);
 
         const payload = twoRelatedTEIs as TEIsPackage;
@@ -34,6 +40,24 @@ describe("TEIsPayloadMapper", () => {
         const mappedPayload = await teiMapper.map(payload);
 
         expect(mappedPayload).toEqual(twoRelatedTEIsMapWithRelationship);
+    });
+    it("should return the payload with mapped orgUnit if mapping contain orgUnits", async () => {
+        const teiMapper = new TEIsPayloadMapper(mappingWithOrgUnits);
+
+        const payload = singleTEI as TEIsPackage;
+
+        const mappedPayload = await teiMapper.map(payload);
+
+        expect(mappedPayload).toEqual(singleTEIMapWithOrgUnit);
+    });
+    it("should return the payload with mapped program if mapping contain tracker programs", async () => {
+        const teiMapper = new TEIsPayloadMapper(mappingTrackerProgramToTrackerProgram);
+
+        const payload = singleTEI as TEIsPackage;
+
+        const mappedPayload = await teiMapper.map(payload);
+
+        expect(mappedPayload).toEqual(singleTEIMapWithProgram);
     });
 });
 
