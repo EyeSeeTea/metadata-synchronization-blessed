4f8e3fe7f8124b3ec339351ef94d48f755448eba
Jorge Sánchez <xurxosf@gmail.com>
2021-03-26 08:26:51 +0100

    Map org unit in the generate event if required

--- src/domain/tracked-entity-instances/mapper/teis-to-event-payload-mapper/__tests__/TEIsToEventPayloadMapper.spec.ts
+++ src/domain/tracked-entity-instances/mapper/teis-to-event-payload-mapper/__tests__/TEIsToEventPayloadMapper.spec.ts
@@ -7,7 +7,8 @@ import { MetadataMappingDictionary } from "../../../../mapping/entities/Metadata
 import teiWithTwoPrograms from "./data/tei/teiWithTwoPrograms.json";
 
 import mappingPrograms from "./data/mapping/mappingPrograms.json";
-import mappingProgramsAndAllAttributes from "./data/mapping/mappingProgramsAndAllAttributes.json";
+import mappingToGenerateEvent from "./data/mapping/mappingToGenerateEvent.json";
+import mappingToGenerateEvent_WithOrgUnit from "./data/mapping/mappingToGenerateEvent_WithOrgUnit.json";
 
 import destinationTwoTrackerPrograms from "./data/programs/destinationTwoTrackerPrograms.json";
 import destinationTwoEventPrograms from "./data/programs/destinationTwoEventPrograms.json";
@@ -16,6 +17,7 @@ import destinatioOneTrackerProgramAndOneEventProgram from "./data/programs/desti
 import emptyEvents from "./data/expected/emptyEvents.json";
 import twoEventsWithAllValues from "./data/expected/twoEventsWithAllValues.json";
 import oneEventWithValues from "./data/expected/oneEventWithValues.json";
+import oneEvent_WithMappedOrgUnit from "./data/expected/oneEvent_WithMappedOrgUnit.json";
 
 describe("TEIsToEventPayloadMapper", () => {
     it("should return empty events if tracker programs are mapped to tracker programs", async () => {
@@ -44,7 +46,7 @@ describe("TEIsToEventPayloadMapper", () => {
     });
     it("should return two events if two tracker programs are mapped to event programs and exist TE Att to DE mapping", async () => {
         const teiMapper = createTEIsToEventPayloadMapper(
-            mappingProgramsAndAllAttributes,
+            mappingToGenerateEvent,
             destinationTwoEventPrograms as ProgramRef[]
         );
 
@@ -56,7 +58,7 @@ describe("TEIsToEventPayloadMapper", () => {
     });
     it("should return one event if one tracker programs are mapped to event program and exist TE Att to DE mapping", async () => {
         const teiMapper = createTEIsToEventPayloadMapper(
-            mappingProgramsAndAllAttributes,
+            mappingToGenerateEvent,
             destinatioOneTrackerProgramAndOneEventProgram as ProgramRef[]
         );
 
@@ -65,6 +67,18 @@ describe("TEIsToEventPayloadMapper", () => {
         const mappedPayload = (await teiMapper.map(teisPayload)) as EventsPackage;
 
         expect(mappedPayload).toEqual(oneEventWithValues);
+    });
+    it("should return one event with a mapped org unit if mapping contains org unit", async () => {
+        const teiMapper = createTEIsToEventPayloadMapper(
+            mappingToGenerateEvent_WithOrgUnit,
+            destinatioOneTrackerProgramAndOneEventProgram as ProgramRef[]
+        );
+
+        const teisPayload = teiWithTwoPrograms as TEIsPackage;
+
+        const mappedPayload = (await teiMapper.map(teisPayload)) as EventsPackage;
+
+        expect(mappedPayload).toEqual(oneEvent_WithMappedOrgUnit);
     });
 });
 
