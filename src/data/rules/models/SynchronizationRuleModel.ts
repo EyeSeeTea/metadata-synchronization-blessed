61bd8c4f215ef997dbca9eec9178d5ab97bbfd25
Alexis Rico <sferadev@gmail.com>
2021-04-08 07:32:31 +0200

    Fix type of SyncRuleModel

--- src/data/rules/models/SynchronizationRuleModel.ts
+++ src/data/rules/models/SynchronizationRuleModel.ts
@@ -1,19 +1,20 @@
-import { Schema } from "../../../utils/codec";
-import { SharedRefModel } from "../../common/models/RefModel";
+import { SynchronizationRuleData } from "../../../domain/rules/entities/SynchronizationRule";
+import { Codec, Schema } from "../../../utils/codec";
+import { NamedRefModel, SharedRefModel } from "../../common/models/RefModel";
 import { SynchronizationBuilderModel } from "../../synchronization/models/SynchronizationBuilderModel";
 import { SynchronizationTypeModel } from "../../synchronization/models/SynchronizationTypeModel";
 
-export const SynchronizationRuleModel = Schema.extend(
+export const SynchronizationRuleModel: Codec<SynchronizationRuleData> = Schema.extend(
     SharedRefModel,
     Schema.object({
-        code: Schema.optionalSafe(Schema.string, ""),
+        code: Schema.optional(Schema.string),
         created: Schema.optionalSafe(Schema.date, () => new Date()),
-        description: Schema.optionalSafe(Schema.string, ""),
+        description: Schema.optional(Schema.string),
         builder: SynchronizationBuilderModel,
         targetInstances: Schema.optionalSafe(Schema.array(Schema.string), []),
         enabled: Schema.optionalSafe(Schema.boolean, false),
         lastExecuted: Schema.optional(Schema.date),
-        lastExecutedBy: Schema.optional(Schema.string),
+        lastExecutedBy: Schema.optional(NamedRefModel),
         frequency: Schema.optional(Schema.string),
         type: SynchronizationTypeModel,
     })
--- src/domain/common/entities/Ref.ts
+++ src/domain/common/entities/Ref.ts
@@ -1,4 +1,5 @@
 import { SharingSetting } from "./SharingSetting";
+
 export interface Ref {
     id: string;
 }
@@ -14,17 +15,17 @@ export interface DatedRef extends NamedRef {
     lastUpdatedBy: NamedRef;
 }
 
-export interface SharedRef extends DatedRef {
-    publicAccess: string;
-    userAccesses: SharingSetting[];
-    userGroupAccesses: SharingSetting[];
-}
-
 export interface IdentifiableRef extends NamedRef {
     shortName?: string;
     code?: string;
     path?: string;
     level?: number;
+}
+
+export interface SharedRef extends DatedRef {
+    publicAccess: string;
+    userAccesses: SharingSetting[];
+    userGroupAccesses: SharingSetting[];
 }
 
 export function updateObject<T extends Ref>(objects: T[], object: T): T[] {
