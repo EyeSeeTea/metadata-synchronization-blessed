1bfd50e3faea8364c11af1254436270877ea698b
Arnau Sanchez <pyarnau@gmail.com>
2021-04-23 13:06:09 +0200

    Refactor relationshipPairs helper

--- src/domain/aggregated/utils.ts
+++ src/domain/aggregated/utils.ts
@@ -3,11 +3,11 @@ import _ from "lodash";
 import { availablePeriods } from "../../utils/synchronization";
 import { DataSynchronizationParams } from "./types";
 
-/* For a map of relationship (child -> parent[]), return the minimum array of parents
+/* For a map of relationships (child -> parent[]), return the minimum array of parents
    that include all of the children.
 
-   Implementation: Invert the original relationship to parent -> child[], and, for every
-   iteration, sort the pairs by length of children, until all children are seen.
+   Implementation: Invert the original relationship and, on every iteration,
+   sort the pairs by the length of children, until all children have been seen.
 */
 export function getMinimumParents(relationships: Map<string, string[]>): string[] {
     const relationshipPairs = _(Array.from(relationships))
@@ -19,29 +19,32 @@ export function getMinimumParents(relationships: Map<string, string[]>): string[
 
     const outputParents: string[] = [];
     const childrenSeen = new Set<string>();
+    let maximum: { parent: string; children: string[] } | undefined;
 
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        const maximum = _.maxBy(relationshipPairs, ([_parent, children]) => children.size);
-        if (!maximum) break;
-
-        const [maxParent, maxChildren] = maximum;
-        if (maxChildren.size === 0) break;
-
-        const maxChildrenArray = Array.from(maxChildren);
+    while ((maximum = getRelationshipWithLongestChildren(relationshipPairs))) {
+        const { parent: maxParent, children: maxChildren } = maximum;
 
         outputParents.push(maxParent);
-        maxChildrenArray.forEach(child => childrenSeen.add(child));
+        maxChildren.forEach(child => childrenSeen.add(child));
 
         // Remove seen children from all the relationships
         relationshipPairs.forEach(([_parent, children]) => {
-            maxChildrenArray.forEach(maxChild => {
+            maxChildren.forEach(maxChild => {
                 children.delete(maxChild);
             });
         });
     }
 
     return outputParents;
+}
+
+function getRelationshipWithLongestChildren(
+    relationshipPairs: Array<[string, Set<string>]>
+): { parent: string; children: string[] } | undefined {
+    const maximum = _.maxBy(relationshipPairs, ([_parent, children]) => children.size);
+    if (!maximum) return;
+    const [parent, children] = maximum;
+    return children.size > 0 ? { parent, children: Array.from(children) } : undefined;
 }
 
 export function buildPeriodFromParams(
