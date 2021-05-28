d09d2cf548d8e94a0aeaa5b70f125102fbb26856
Jocelyn Dunkley <jocelyn.dunkley09@gmail.com>
2021-05-28 12:15:10 +0200

    added getById and save code but I'm getting an HTTP 500 error

--- src/data/instance/InstanceFileD2Repository.ts
+++ src/data/instance/InstanceFileD2Repository.ts
@@ -4,94 +4,93 @@ import {
 } from "../../domain/instance/repositories/InstanceFileRepository";
 import { Instance } from "../../domain/instance/entities/Instance";
 import mime from "mime-types";
-import { D2Document } from "../../types/d2-api";
+import { D2Api } from "../../types/d2-api";
+import { getD2APiFromInstance } from "../../utils/d2-utils";
+import { getUid } from "./uid";
 
-interface SaveApiResponse {
+/*interface SaveApiResponse {
     response: {
         fileResource: {
             id: string;
         };
     };
-}
+}*/
 
 export class InstanceFileD2Repository implements InstanceFileRepository {
-    constructor(private instance: Instance) {}
+    private api: D2Api;
+    public baseUrl: string;
+
+    constructor(private instance: Instance) {
+        this.api = getD2APiFromInstance(instance);
+        this.baseUrl = this.api.baseUrl;
+    }
+
 
     public async getById(fileId: FileId): Promise<File> {
         const auth = this.instance.auth;
 
-        const authHeaders: Record<string, string> = this.getAuthHeaders(auth);
+        const authHeaders: Record<string, string> = auth
+            ? { Authorization: "Basic " + btoa(auth.username + ":" + auth.password) }
+            : {};
 
         const fetchOptions: RequestInit = {
             method: "GET",
-            headers: { ...authHeaders },
-            credentials: auth ? "omit" : ("include" as const),
+            headers: authHeaders,
+            credentials: auth ? "omit" : "include",
         };
 
-        const documentResponse = await fetch(
-            new URL(`/api/documents/${fileId}`, this.instance.url).href,
-            fetchOptions
-        );
-
-        const document: D2Document = JSON.parse(await documentResponse.text());
-
-        const response = await fetch(
-            new URL(`/api/documents/${fileId}/data`, this.instance.url).href,
-            fetchOptions
-        );
+        const response = await fetch(`${this.baseUrl}/api/documents/${fileId}/data`, fetchOptions);
 
         if (!response.ok) {
-            throw Error(
-                `An error has ocurred retrieving the file resource of document '${document.name}' from ${this.instance.name}`
-            );
+            throw Error("An error has ocurred retrieving the file resource of document");
         } else {
             const blob = await response.blob();
 
-            return this.blobToFile(blob, `${document.name}.${mime.extension(blob.type)}`);
+            return this.blobToFile(blob, `document1.${mime.extension(blob.type)}`);
         }
     }
 
     public async save(file: File): Promise<FileId> {
-        const auth = this.instance.auth;
+        const fileToBlob = new Blob([new Uint8Array(await file.arrayBuffer())], {type: file.type });
 
-        const authHeaders: Record<string, string> = this.getAuthHeaders(auth);
+        const ff = await new Promise<string>((resolve, reject) => {
+            const reader = new FileReader();
 
-        const formdata = new FormData();
-        formdata.append("file", file);
-        formdata.append("filename", file.name);
+            reader.onload = ev => {
+                if (ev.target) {
+                    resolve(ev.target.result as string);
+                } else {
+                    reject(new Error("Could not convert array to string!"));
+                }
+            };
+            reader.readAsText(fileToBlob, "UTF-8");
+        });
+        const { id } = await this.api.files
+            .upload({
+                id: getUid(ff),
+                name: file.name,
+                data: fileToBlob,
+            })
+            .getData();
+        console.log(`here is the ID: ${id}`);
 
-        const fetchOptions: RequestInit = {
-            method: "POST",
-            headers: { ...authHeaders },
-            body: formdata,
-            credentials: auth ? "omit" : ("include" as const),
-        };
+        if (!id) {
+            //const responseBody = JSON.parse(await response.text());
 
-        const response = await fetch(
-            new URL(`/api/fileResources`, this.instance.url).href,
-            fetchOptions
-        );
-        if (!response.ok) {
-            const responseBody = JSON.parse(await response.text());
-
-            const bodyError = responseBody.message ? `: ${responseBody.message}` : "";
+            //const bodyError = responseBody.message ? `: ${responseBody.message}` : "";
 
             throw Error(
-                `An error has ocurred saving the resource file of the document '${file.name}' in ${this.instance.name}${bodyError}`
+                `An error has ocurred saving the resource file of the document '${file.name}' in ${this.instance.name}`
             );
         } else {
-            const apiResponse: SaveApiResponse = JSON.parse(await response.text());
+            //return `${this.api.apiPath}/documents/${id}/data`;
 
-            return apiResponse.response.fileResource.id;
+            //const apiResponse: SaveApiResponse = JSON.parse(await response.text());
+
+            return id;
         }
     }
-
-    private getAuthHeaders(
-        auth: { username: string; password: string } | undefined
-    ): Record<string, string> {
-        return auth ? { Authorization: "Basic " + btoa(auth.username + ":" + auth.password) } : {};
-    }
-
+    
     private blobToFile = (blob: Blob, fileName: string): File => {
         return new File([blob], fileName, { type: blob.type });
     };
