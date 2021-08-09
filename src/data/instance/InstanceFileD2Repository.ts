984433dd9d996a4ffc4c44f8512fc4fb6753108d
Alexis Rico <sferadev@gmail.com>
2021-08-09 10:06:54 +0200

    Remove console logs

--- src/data/instance/InstanceFileD2Repository.ts
+++ src/data/instance/InstanceFileD2Repository.ts
@@ -1,12 +1,11 @@
+import mime from "mime-types";
+import { Instance } from "../../domain/instance/entities/Instance";
 import {
     FileId,
     InstanceFileRepository,
 } from "../../domain/instance/repositories/InstanceFileRepository";
-import { Instance } from "../../domain/instance/entities/Instance";
-import mime from "mime-types";
 import { D2Api } from "../../types/d2-api";
 import { getD2APiFromInstance } from "../../utils/d2-utils";
-import { debug } from "../../utils/debug";
 
 export class InstanceFileD2Repository implements InstanceFileRepository {
     private api: D2Api;
@@ -16,22 +15,17 @@ export class InstanceFileD2Repository implements InstanceFileRepository {
     }
 
     public async getById(fileId: FileId): Promise<File> {
-        debug("fileId: ", fileId);
         const response = await this.api.files.get(fileId).getData();
-        debug("Response: ", response);
+        if (!response) throw Error("An error has ocurred retrieving the file resource of document");
 
-        if (!response) {
-            throw Error("An error has ocurred retrieving the file resource of document");
-        } else {
-            const documentName = await this.api.models.documents
-                .get({ filter: { id: { eq: fileId } }, fields: { name: true } })
-                .getData();
-            debug("documentName: ", documentName);
-            return this.blobToFile(
-                response,
-                `${documentName.objects[0].name}.${mime.extension(response.type)}`
-            );
-        }
+        const documentName = await this.api.models.documents
+            .get({ filter: { id: { eq: fileId } }, fields: { name: true } })
+            .getData();
+
+        return this.blobToFile(
+            response,
+            `${documentName.objects[0].name}.${mime.extension(response.type)}`
+        );
     }
 
     public async save(file: File): Promise<FileId> {
