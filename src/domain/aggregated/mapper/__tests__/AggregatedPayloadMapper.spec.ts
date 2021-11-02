43501aa89d6867b920d9fceb82cf73dc85af0781
Jorge Sánchez <xurxosf@gmail.com>
2021-11-02 11:21:51 +0100

    Add test for org units mapping

--- src/domain/aggregated/mapper/__tests__/AggregatedPayloadMapper.spec.ts
+++ src/domain/aggregated/mapper/__tests__/AggregatedPayloadMapper.spec.ts
@@ -6,7 +6,10 @@ import destinationCategoryOptionCombos from "./data/category_option_combos/desti
 
 import dataValues from "./data/data-values/dataValues.json";
 
+import orgUnitsMapping from "./data/mapping/mapping_orgUnits.json";
+
 import dataValuesWithoutMapping from "./data/expected/dataValues_without_mapping.json";
+import dataValuesOrgUnitsMapping from "./data/expected/dataValues_orgunits_mapping.json";
 
 describe("AggreggatedPayloadMapper", () => {
     it("should return the expected payload if mapping is empty", async () => {
@@ -15,6 +18,13 @@ describe("AggreggatedPayloadMapper", () => {
         const mappedPayload = await aggregatedMapper.map(dataValues);
 
         expect(mappedPayload).toEqual(dataValuesWithoutMapping);
+    });
+    it("should return the payload with mapped orgUnit if mapping contain orgUnits", async () => {
+        const aggregatedMapper = createAggregatedPayloadMapper(orgUnitsMapping);
+
+        const mappedPayload = await aggregatedMapper.map(dataValues);
+
+        expect(mappedPayload).toEqual(dataValuesOrgUnitsMapping);
     });
 });
 
