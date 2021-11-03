3f5ccef829f08c63d001359d72be2a8d62af801b
Jorge Sánchez <xurxosf@gmail.com>
2021-11-03 12:04:29 +0100

    Add tests for category option mapping

--- src/domain/aggregated/mapper/__tests__/AggregatedPayloadMapper.spec.ts
+++ src/domain/aggregated/mapper/__tests__/AggregatedPayloadMapper.spec.ts
@@ -8,10 +8,14 @@ import dataValues from "./data/data-values/dataValues.json";
 
 import orgUnitsMapping from "./data/mapping/mapping_orgUnits.json";
 import dataElementsMapping from "./data/mapping/mapping_dataelements.json";
+import deCategoryOptionMapping from "./data/mapping/mapping_de_category_option.json";
+import globalCategoryOptionMapping from "./data/mapping/mapping_global_category_option.json";
 
 import dataValuesWithoutMapping from "./data/expected/dataValues_without_mapping.json";
 import dataValuesOrgUnitsMapping from "./data/expected/dataValues_orgunits_mapping.json";
 import dataValuesDataElementsMapping from "./data/expected/dataValues_dataelements_mapping.json";
+import dataValuesDECategoryOptionMapping from "./data/expected/dataValues_de_category_option_mapping.json";
+import dataValuesGlobalCategoryOptionMapping from "./data/expected/dataValues_global_category_option_mapping.json";
 
 describe("AggreggatedPayloadMapper", () => {
     it("should return the expected payload if mapping is empty", async () => {
@@ -34,6 +38,20 @@ describe("AggreggatedPayloadMapper", () => {
         const mappedPayload = await aggregatedMapper.map(dataValues);
 
         expect(mappedPayload).toEqual(dataValuesDataElementsMapping);
+    });
+    it("should return the payload with mapped category option if mapping contain category option by DE", async () => {
+        const aggregatedMapper = createAggregatedPayloadMapper(deCategoryOptionMapping);
+
+        const mappedPayload = await aggregatedMapper.map(dataValues);
+
+        expect(mappedPayload).toEqual(dataValuesDECategoryOptionMapping);
+    });
+    it("should return the payload with mapped category option if mapping contain global category option", async () => {
+        const aggregatedMapper = createAggregatedPayloadMapper(globalCategoryOptionMapping);
+
+        const mappedPayload = await aggregatedMapper.map(dataValues);
+
+        expect(mappedPayload).toEqual(dataValuesGlobalCategoryOptionMapping);
     });
 });
 
