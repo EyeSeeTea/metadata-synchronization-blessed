8d841a61f228c150e7875dca9a0be90968127489
xurxodev <xurxosf@gmail.com>
2022-12-23 11:11:30 +0100

    Fix bug to try apply 2.38 user transformations without users

--- src/data/transformations/PackageTransformations.ts
+++ src/data/transformations/PackageTransformations.ts
@@ -7,7 +7,7 @@ export const metadataTransformations: Transformation[] = [
         apply: ({ users, ...rest }: any) => {
             return {
                 ...rest,
-                users: users.map((user: any) => ({ ...user, userRoles: user.userCredentials.userRoles })),
+                users: users?.map((user: any) => ({ ...user, userRoles: user.userCredentials.userRoles })) || undefined,
             };
         },
     },
