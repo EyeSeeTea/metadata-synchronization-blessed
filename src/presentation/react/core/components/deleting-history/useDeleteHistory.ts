d4a591e464e9ac86ec7eb93e776f07a72f54a131
xurxodev <xurxosf@gmail.com>
2024-02-21 12:14:35 +0100

    Format the code

--- src/presentation/react/core/components/deleting-history/useDeleteHistory.ts
+++ src/presentation/react/core/components/deleting-history/useDeleteHistory.ts
@@ -5,23 +5,21 @@ export function useDeleteHistory(appContext: AppContextState | null) {
     const [deletingHistory, setDeletingHistory] = useState<boolean | undefined>();
 
     useEffect(() => {
-        if (!appContext) return 
+        if (!appContext) return;
 
         appContext.compositionRoot.settings.get().then(settings => {
-            if (settings.historyRetentionDays){
+            if (settings.historyRetentionDays) {
                 setDeletingHistory(true);
 
                 appContext.compositionRoot.reports
-                .deleteOld()
-                .then(() => setDeletingHistory(false))
-                .catch(error => {
-                    console.debug(error);
-                    setDeletingHistory(false);
-                });
+                    .deleteOld()
+                    .then(() => setDeletingHistory(false))
+                    .catch(error => {
+                        console.debug(error);
+                        setDeletingHistory(false);
+                    });
             }
-        })
-
-       
+        });
     }, [appContext]);
 
     return { deletingHistory };
