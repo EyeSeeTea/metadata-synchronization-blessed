6d84394e19eb4b1170ae94198cd1fe0a866d5b7b
Ana Garcia <ana@eyeseetea.com>
2025-03-05 16:20:58 +0100

    Update Future entity and apiToFuture function

---
+++ src/data/common/utils/api-futures.ts
@@ -0,0 +1,18 @@
+import { Future, FutureData } from "../../../domain/common/entities/Future";
+import { CancelableResponse } from "../../../types/d2-api";
+
+export function apiToFuture<Data>(res: CancelableResponse<Data>): FutureData<Data> {
+    return Future.fromComputation((resolve, reject) => {
+        res.getData()
+            .then(resolve)
+            .catch((err: unknown) => {
+                if (err instanceof Error) {
+                    reject(err);
+                } else {
+                    console.error("apiToFuture:uncatched", err);
+                    reject(new Error("Unknown error"));
+                }
+            });
+        return res.cancel;
+    });
+}
