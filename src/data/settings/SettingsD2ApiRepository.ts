1af7e27058fb65cd3ca73721449dd951b7bdcc74
9sneha-n <9sneha.n@gmail.com>
2025-03-31 23:32:44 +0530

    fix: set default history retention to 30 days

--- src/data/settings/SettingsD2ApiRepository.ts
+++ src/data/settings/SettingsD2ApiRepository.ts
@@ -1,4 +1,4 @@
-import { FutureData } from "../../domain/common/entities/Future";
+import { Future, FutureData } from "../../domain/common/entities/Future";
 import { Settings, SettingsData } from "../../domain/settings/Settings";
 import { SettingsRepository } from "../../domain/settings/SettingsRepository";
 import { StorageClient } from "../../domain/storage/repositories/StorageClient";
@@ -7,10 +7,25 @@ import { Namespace } from "../storage/Namespaces";
 
 export class SettingsD2ApiRepository implements SettingsRepository {
     get(storageClient: StorageClient): FutureData<Settings> {
-        return storageClient.getObjectFuture<SettingsData>(Namespace.SETTINGS).map(settingsData => {
-            return settingsData
-                ? Settings.create({ historyRetentionDays: settingsData.historyRetentionDays?.toString() }).getOrThrow()
-                : Settings.create({ historyRetentionDays: undefined }).getOrThrow();
+        return storageClient.getObjectFuture<SettingsData>(Namespace.SETTINGS).flatMap(settingsData => {
+            if (!settingsData) {
+                const defaultSettings = {
+                    historyRetentionDays: 30,
+                }; //Set default history retention days to 30 days
+                return storageClient.saveObjectFuture<SettingsData>(Namespace.SETTINGS, defaultSettings).flatMap(() => {
+                    return Future.success(
+                        Settings.create({
+                            historyRetentionDays: defaultSettings.historyRetentionDays?.toString(),
+                        }).getOrThrow()
+                    );
+                });
+            } else {
+                return Future.success(
+                    Settings.create({
+                        historyRetentionDays: settingsData.historyRetentionDays?.toString(),
+                    }).getOrThrow()
+                );
+            }
         });
     }
 
